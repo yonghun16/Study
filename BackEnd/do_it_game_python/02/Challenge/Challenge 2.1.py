839242a5718ea68029cccc7f86ef1f1204770f1a
yonghun16 <yonghun16@naver.com>
2023-03-22 22:14:31 +0900

    study update

---
+++ BackEnd/do_it_game_python/02/Challenge/Challenge 2.1.py
@@ -0,0 +1,4 @@
+firstName = input("성이 무엇인가요? ")
+lastName = input("이름이 무엇인가요? ")
+
+print("안녕하세요,", firstName, lastName, "님. 반갑습니다.")
---
+++ BackEnd/do_it_game_python/02/Hello4.py
@@ -0,0 +1,5 @@
+firstName = "쉬무엘"
+lastName = "포터"
+fullName = firstName + " " + lastName
+
+print("안녕하세요, 저는", fullName, "이며 프로그래머랍니다!")
