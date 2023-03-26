2bdd0f98e6a6ad19c3376e59b72cd46051a63898
yonghun16 <yonghun16@naver.com>
2023-03-26 21:31:11 +0900

    python study

---
+++ BackEnd/do_it_game_python/04/Challenge/challenge 4.1.py
@@ -0,0 +1,9 @@
+# 라이브러리 불러오기
+import datetime
+
+# 오늘 날짜 가져오기
+today = datetime.datetime.now()
+
+# 오늘의 연도, 월, 일 출력하기
+print("지금은", today.hour, "시 입니다.")
+print("지금은", today.minute, "분 입니다.")
---
+++ BackEnd/do_it_game_python/04/Date2.py
@@ -0,0 +1,10 @@
+# 라이브러리 불러오기
+import datetime
+
+# 오늘 날짜 가져오기
+today = datetime.datetime.now()
+
+# 오늘의 연도, 월, 일 출력하기
+print("지금은", today.year, "년입니다.")
+print("지금은", today.month, "월입니다.")
+print("지금은", today.day, "일입니다.")
