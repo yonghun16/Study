4d7bd77ff173c9c7957d5f69c68ce61c1c7d4fce
yonghun16 <yonghun16@naver.com>
2023-04-26 01:12:48 +0900

    upload

---
+++ BackEnd/studying_alone_of_Python/Chapter04/iterator01.py
@@ -0,0 +1,11 @@
+# reversed() 함수와 이터레이터
+numbers = [1, 2, 3, 4, 5, 6]
+r_num = reversed(numbers)
+
+# reversed_numbers를 출력합니다.
+print("reversed_numbers :", r_num)
+print(next(r_num))
+print(next(r_num))
+print(next(r_num))
+print(next(r_num))
+print(next(r_num))
