3c5e930199c0c0bfb013c93b52fcfe9d755d13c4
yonghun16 <yonghun16@naver.com>
2023-05-06 23:00:10 +0900

    study upload

---
+++ BackEnd/studying_alone_of_Python/Chapter08/compare_func.py
@@ -0,0 +1,53 @@
+# 클래스를 선언합니다.
+class Student:
+    def __init__(self, name, korean, math, english, science):
+        self.name = name
+        self.korean = korean
+        self.math = math
+        self.english = english
+        self.science = science
+
+    def get_sum(self):
+        return self.korean + self.math +\
+                self.english + self.science
+
+    def get_average(self):
+        return self.get_sum() / 4
+
+    def __str__(self):
+        return "{}\t{}\t{}".format(
+                self.name,
+                self.get_sum(),
+                self.get_average()
+                )
+
+    def __eq__(self, value):
+        return self.get_sum() == value.get_sum()
+    def __ne__(self, value):
+        return self.get_sum() != value.get_sum()
+    def __gt__(self, value):
+        return self.get_sum() > value.get_sum()
+    def __ge__(self, value):
+        return self.get_sum() >= value.get_sum()
+
+# 학생 리스트를 선언합니다.
+students = [
+        Student("윤인성", 87, 92, 88, 95),
+        Student("연하진", 92, 55, 32, 95),
+        Student("구지연", 76, 87, 44, 93),
+        Student("나선주", 57, 90, 88, 95),
+        Student("윤아린", 87, 84, 66, 92),
+        Student("윤명월", 87, 98, 78, 85),
+        ]
+
+# 학생을 선언합니다.
+student_a = Student("윤인성", 87, 98, 88, 95),
+student_b = Student("윤인성", 92, 98, 96, 98),
+
+# 출력합니다.
+print("student_a == student_b = ", student_a == student_b)
+print("student_a != student_b = ", student_a != student_b)
+print("student_a >  student_b = ", student_a >  student_b)
+print("student_a >= student_b = ", student_a >= student_b)
+print("student_a <  student_b = ", student_a <  student_b)
+print("student_a <= student_b = ", student_a <= student_b)
---
+++ BackEnd/studying_alone_of_Python/Chapter08/str_func.py
@@ -0,0 +1,38 @@
+#  클래스를 선언합니다.
+class Student:
+    def __init__(self, name, korean, math, english, science):
+        self.name = name
+        self.korean = korean
+        self.math = math
+        self.english = english
+        self.science = science
+
+    def get_sum(self):
+        return self.korean + self.math +\
+                self.english + self.science
+
+    def get_average(self):
+        return self.get_sum() / 4
+
+    def __str__(self):
+        return "{}\t{}\t{}".format(
+                self.name,
+                self.get_sum(),
+                self.get_average()
+                )
+
+# 학생 리스트를 선언합니다.
+students = [
+        Student("윤인성", 87, 92, 88, 95),
+        Student("연하진", 92, 55, 32, 95),
+        Student("구지연", 76, 87, 44, 93),
+        Student("나선주", 57, 90, 88, 95),
+        Student("윤아린", 87, 84, 66, 92),
+        Student("윤명월", 87, 98, 78, 85),
+        ]
+
+# 학생을 한 명씩 반복합니다.
+print("이름", "총점", "평균", sep="\t")
+for student in students:
+    # 출력합니다.
+    print(str(student))
