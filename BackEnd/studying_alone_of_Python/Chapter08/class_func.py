aa8daae10bb641a904681b590bdc7e0f3b1d73b8
yonghun16 <yonghun16@naver.com>
2023-05-06 23:34:53 +0900

    python study

---
+++ BackEnd/studying_alone_of_Python/Chapter08/class_func.py
@@ -0,0 +1,46 @@
+# 클래스를 선언합니다.
+class Student:
+    # 클래스 변수
+    count = 0
+    students = []
+
+    # 클래스 함수
+    @classmethod
+    def print(cls):
+        print("------ 학생목록 ------")
+        print("이름\t총점\t평균")
+        for student in cls.students:
+            print(str(student))
+        print("------ ------ ------")
+
+    # 인스턴스 함수 
+    def __init__(self, name, korean, math, english, science):
+        self.name = name
+        self.korean = korean
+        self.math = math
+        self.english = english
+        self.science = science
+        Student.count += 1
+        Student.students.append(self)
+
+    def get_sum(self):
+        return self.korean + self.math +\
+                self.english + self.science
+
+    def get_average(self):
+        return self.get_sum() /4
+
+    def __str__(self):
+        return "{}\t{}\t{}".format(\
+                self.name, self.get_sum(), self.get_average())
+
+# 학생 리스트를 선언합니다.
+Student("윤인성", 87, 92, 88, 95),
+Student("연하진", 92, 55, 32, 95),
+Student("구지연", 76, 87, 44, 93),
+Student("나선주", 57, 90, 88, 95),
+Student("윤아린", 87, 84, 66, 92),
+Student("윤명월", 87, 98, 78, 85),
+
+# 현재 생성된 학생을 모두출력합니다.
+Student.print()
---
+++ BackEnd/studying_alone_of_Python/Chapter08/class_var.py
@@ -0,0 +1,29 @@
+# 클래스를 선언합니다.
+class Student:
+    count = 0
+
+    def __init__(self, name, korean, math, english, science):
+        # 인스턴스 변수 초기화
+        self.name = name
+        self.korean = korean
+        self.math = math
+        self.english = english
+        self.science = science
+
+        # 클래스 변수 설정
+        Student.count += 1
+        print("{}번째 학생이 생성되었습니다.".format(Student.count))
+
+# 학생 리스트를 선언합니다.
+students = [
+        Student("윤인성", 87, 92, 88, 95),
+        Student("연하진", 92, 55, 32, 95),
+        Student("구지연", 76, 87, 44, 93),
+        Student("나선주", 57, 90, 88, 95),
+        Student("윤아린", 87, 84, 66, 92),
+        Student("윤명월", 87, 98, 78, 85),
+        ]
+
+# 출력합니다.
+print()
+print("현재 생성된 총 학생 수는 {}명입니다.".format(Student.count))
