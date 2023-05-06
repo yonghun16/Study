1066558a0f38fb6743559d35a30d5c4db5f36a29
yonghun16 <yonghun16@naver.com>
2023-05-07 00:53:11 +0900

    Create inherit01.py

---
+++ BackEnd/studying_alone_of_Python/Chapter08/inherit01.py
@@ -0,0 +1,18 @@
+# 부모 클래스를 선언합니다.
+class Parent:
+    def __init__(self):
+        self.value = "테스트"
+        print("Parent 클래스의 __init()__ 메소드가 호출되었습니다.")
+    def test(self):
+        print("Parent 클래스의 test() 메소드입니다.")
+
+# 자식 클래스를 선언합니다.
+class Child(Parent):
+    def __init__(self):
+        Parent.__init__(self)
+        print("Child 클래스의 __init()__ 메소드가 호출되었습니다.")
+
+# 자식 클래스의 인스턴스를 생성하고 메소드를 호출합니다.
+child = Child()
+child.test()
+print(child.value)
