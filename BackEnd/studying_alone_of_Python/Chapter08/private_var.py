ce45a292e199e7141f7c7573ab84c72ef2c0798a
yonghun16 <yonghun16@naver.com>
2023-05-07 00:44:38 +0900

    study upload

--- BackEnd/studying_alone_of_Python/Chapter08/private_var.py
+++ BackEnd/studying_alone_of_Python/Chapter08/private_var.py
@@ -4,13 +4,19 @@ import math
 # 클래스를 선언합니다.
 class Circle:
     def __init__(self, radius):
-        self.radius = radius
+        self.__radius = radius
     def get_circumference(self):
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * self.__radius
     def get_area(self):
-        return math.pi * (self.radius ** 2)
+        return math.pi * (self.__radius ** 2)
 
 # 원의 둘레와 넓이를 구합니다.
 circle = Circle(10)
+print("# 원의 둘레와 넓이를 구합니다.")
 print("원의 둘레:", circle.get_circumference())
 print("원의 넓이:", circle.get_area())
+print()
+
+# __radius에 접근합니다.
+print("# __radius에 접근합니다.")
+print(circle.__radius)
