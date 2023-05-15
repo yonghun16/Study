8b5dfc2d558a30e3c05bb8095893b3b1bf8b2560
yonghun16 <yonghun16@naver.com>
2023-05-15 21:59:37 +0900

    update

---
+++ BackEnd/Programming_basic/08.string/9528.py
@@ -0,0 +1,2 @@
+data = input().split()
+print("{}의 키는 {}cm이며, 몸무게는 {:.1f}kg입니다.".format(data[0], data[1], float(data[2])))
---
+++ BackEnd/Programming_basic/08.string/993.py
@@ -0,0 +1,5 @@
+a = input()
+b = input()
+c = float(input())
+
+print("I am {}(IDNo. {}). I got {:.6f} in my midterm exam.".format(a, b, c))
