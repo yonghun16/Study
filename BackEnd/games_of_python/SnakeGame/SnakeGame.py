6b260f8ee1179abb4d50c13d0db70675a637ae50
yonghun16 <yonghun16@naver.com>
2023-05-15 15:56:29 +0900

    update

--- BackEnd/games_of_python/SnakeGame/SnakeGame.py
+++ BackEnd/games_of_python/SnakeGame/SnakeGame.py
@@ -24,16 +24,17 @@ WHITE = (255, 255, 255)
 ORANGE = (250, 150, 0)
 GRAY = (100, 100, 100)
 
+
 # 뱀 객체
-class Snake():
+class Snake(object):
     def __init__(self):
         self.create()
 
     # 뱀 생성
     def create(self):
-        self.length = 2     # 생성 시 길이 2
-        self.positions = [(int(SCREEN_WIDTH/2), int(SCREEN_HEIGHT/2))]  # 생성 시 시작 위치
-        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])         # 생성 시 방향
+        self.length = 2
+        self.positions = [ (int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2)) ]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
 
     # 뱀 방향 조정
     def control(self, xy):
@@ -41,36 +42,145 @@ class Snake():
             return
         else:
             self.direction = xy
-    # 뱀 이동
-    # def move(self):
-    #     cur = self.positions[0]
 
+    # 뱀 이동
+    def move(self):
+        cur = self.positions[0]
+        x, y = self.direction
+        new = (cur[0] + (x * GRID_SIZE)), (cur[1] + (y * GRID_SIZE))
+
+        # 뱀이 자기 몸통에 닿았을 경우 뱀 처음부터 다시 생성
+        if new in self.positions[2:]:
+            sleep(1)
+            self.create()
+        # 뱀이 게임화면을 넘어갈 경우 뱀 처음부터 다시 생성
+        elif new[0] < 0 or new[0] >= SCREEN_WIDTH or \
+                new[1] < 0 or new[1] >= SCREEN_HEIGHT:
+            sleep(1)
+            self.create()
+        # 뱀이 정상적으로 이동하는 경우
+        else:
+            self.positions.insert(0, new)           # 현재 뱀'머리'에 새로 이동한 좌표를 추가
+            if len(self.positions) > self.length:   # 추가된 뱀 길이가 현재 뱀길이 보다 길면
+                self.positions.pop()                # 마지막 뱀의 꼬리를 삭제함
+
+    # 뱀이 먹이를 먹을 때 호출
+    def eat(self):
+        self.length += 1
+
+    # 뱀 그리기
+    def draw(self, screen):
+        red, green, blue = int(50 / (self.length - 1)), 150, int(150 / (self.length - 1))
+        for i, p in enumerate(self.positions):
+            color = (100 + red * i, green, blue * i)
+            rect = pygame.Rect( (p[0], p[1]), (GRID_SIZE, GRID_SIZE) )
+            pygame.draw.rect(screen, color, rect, 0)
+
+# 먹이 객체
+class Feed():
+    def __init__(self):
+        self.position = (0, 0)
+        self.color = ORANGE
+        self.create()
+
+    # 먹이 생성
+    def create(self):
+        x = random.randint(0, int(GRID_WIDTH - 1))
+        y = random.randint(0, int(GRID_HEIGHT - 1))
+        self.position = x * GRID_SIZE, y * GRID_SIZE
+
+    # 먹이 그리기
+    def draw(self, screen):
+        rect = pygame.Rect((self.position[0], self.position[1]), (GRID_SIZE, GRID_SIZE))
+        pygame.draw.rect(screen, self.color, rect, 0)
+
+
+# 게임 객체
+class Game():
+    def __init__(self):
+        self.snake = Snake()
+        self.feed = Feed()
+        self.first_speed = 20
+
+    # 게임 이벤트 처리 및 조작
+    def process_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.snake.control(UP)
+                elif event.key == pygame.K_DOWN:
+                    self.snake.control(DOWN)
+                elif event.key == pygame.K_LEFT:
+                    self.snake.control(LEFT)
+                elif event.key == pygame.K_RIGHT:
+                    self.snake.control(RIGHT)
+        return False
+
+    # 게임 로직 수행
+    def run_logic(self):
+        self.snake.move()
+        self.check_eat(self.snake, self.feed)
+        self.speed = (self.first_speed + self.snake.length) / 4
+
+    # 뱀이 먹이를 먹었는지 체크
+    def check_eat(self, snake, feed):
+        if snake.positions[0] == feed.position:     # 뱀의머리(position[0])의 위치가 먹이의 위치와 같다면
+            snake.eat()
+            feed.create()
+
+    def resource_path(self, relative_path):
+        try:
+            base_path = sys._MEIPASS
+        except Exception:
+            base_path = os.path.abspath(".")
+        return os.path.join(base_path, relative_path)
+
+    # 게임 정보 출력
+    def draw_info(self, length, speed, screen):
+        info = "Length: " + str(length) + "    " + "Speed: " + str(round(speed, 2))
+        font_path = resource_path("assets/NanumGothicCoding-Bold.ttf")
+        font = pygame.font.Font(font_path, 26)
+        text_obj = font.render(info, True, GRAY)
+        text_rect = text_obj.get_rect()
+        text_rect.x, text_rect.y = 10, 10
+        screen.blit(text_obj, text_rect)
+
+    # 게임 프레임 처리
+    def display_frame(self, screen):
+        screen.fill(WHITE)
+        self.draw_info(self.snake.length, self.speed, screen)
+        self.snake.draw(screen)
+        self.feed.draw(screen)
+        screen.blit(screen, (0, 0))
+
+# 리소스 경로 설정
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 def main():
-    # 초기화
+    # 게임 초기화 및 환경 설정
     pygame.init()
     pygame.display.set_caption('Snake Game')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    game = Game()
 
     done = False
     while not done:
-        # 윈도우 화면 채우기
-        screen.fill(WHITE)
-
-        # 이벤트 반복 구간
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-
-        # 화면 업데이트
+        done = game.process_events()
+        game.run_logic()
+        game.display_frame(screen)
         pygame.display.flip()
+        clock.tick(game.speed)
 
-        # 초당 60 프레임으로 업데이트
-        clock.tick(60)
-
-    # 게임 종료
     pygame.quit()
+
 
 if __name__ == '__main__':
     main()
---
+++ BackEnd/games_of_python/SnakeGame/test/position.py
@@ -0,0 +1,30 @@
+# position 리스트의 변동 사항 테스트
+position = []
+position.append([4, 101])
+position.append([5, 102])
+position.append([6, 103])
+position.append([7, 104])
+position.append([8, 105])
+
+print(position)             # position 전체 출력
+print(position[0])          # position 첫 번째 원소 출력
+print(position[0][1])       # position 첫 번째 원소안의 두 번째 원소 출력
+print()
+
+# 고전적 출력 방식 (c, java 등)
+for i in range(len(position)):
+    for j in range(len(position[i])):
+        print(position[i][j], end=' ')
+    print()
+print()
+
+# enumerate함수 사용(전달 받은 position리스트 사용) (인덱스 및 전달 받은 리스트 사용)
+# (enumerate함수는 함수 안에 반복문이 삽입되어있음)
+for i, bxy in enumerate(position):
+    print(position[i][0], position[i][1])
+print()
+
+# enumerate함수 사용('인덱스'와 '원소'로 '튜플' 생성) (인덱스 안사용, 튜플 사용)
+for i, bxy in enumerate(position):
+    print(bxy[0], bxy[1])
+
