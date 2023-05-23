1dd87960bee2927eb734da1d77ac5d7d90800558
yonghun16 <yonghun16@naver.com>
2023-05-23 22:05:39 +0900

    study up

--- FrontEnd/Nomad_coders/momentum-clone/app.js
+++ FrontEnd/Nomad_coders/momentum-clone/app.js
@@ -1,11 +1,4 @@
-const h1 = document.querySelector("div.hello:last-child")
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button")
 
-console.log(h1)
-
-
-// function handleTitleClick() {
-//   h1.className = "active";
-// }
-
-// h1.addEventListener("click", handleTitleClick)
-
+loginButton.addEventListener("click", )
