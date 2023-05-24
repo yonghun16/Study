98a2135826594b1785a9d871b5028ad7c8690c25
yonghun16 <yonghun16@naver.com>
2023-05-24 12:40:58 +0900

    Update app.js

--- FrontEnd/Nomad_coders/momentum-clone/app.js
+++ FrontEnd/Nomad_coders/momentum-clone/app.js
@@ -1,4 +1,8 @@
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button")
+const loginButton = document.querySelector("#login-form button");
 
-loginButton.addEventListener("click", )
+function onLoginBtnClick() {
+  console.log("click")
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
