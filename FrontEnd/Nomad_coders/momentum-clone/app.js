e8db9ea8be69273562d6a8020f16d1b785d88b30
yonghun16 <yonghun16@naver.com>
2023-05-25 20:44:48 +0900

    study up

--- FrontEnd/Nomad_coders/momentum-clone/app.js
+++ FrontEnd/Nomad_coders/momentum-clone/app.js
@@ -1,8 +1,15 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {
-  console.log("click")
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
