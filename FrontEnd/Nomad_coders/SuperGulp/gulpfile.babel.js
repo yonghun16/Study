f9225a3e6bf6fc53fd88313befba248fe6fe4af7
yonghun16 <yonghun16@naver.com>
2023-06-04 11:14:45 +0900

    study up

--- FrontEnd/Nomad_coders/SuperGulp/gulpfile.babel.js
+++ FrontEnd/Nomad_coders/SuperGulp/gulpfile.babel.js
@@ -3,40 +3,66 @@ import gpug from "gulp-pug";
 import del from "del";
 import image from "gulp-image";
 import ws from "gulp-webserver";
+const sass = require("gulp-sass")(require("node-sass"));
+import autop from "gulp-autoprefixer";
+
+sass.compiler = require("node-sass");
 
 const routes = {
   pug: {
+    watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build"
   },
   img: {
     src: "src/img/*",
     dest: "build/img"
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css"
   }
-}
+};
 
 const pug = () => 
   gulp.src(routes.pug.src)
   .pipe(gpug())
   .pipe(gulp.dest(routes.pug.dest));
 
-const webserver = () =>
-  gulp.src("build")
-  .pipe(ws({livereload:true, open:true}))
-
 const clean = () => 
   del(["build/"]);
+
+const webserver = () =>
+  gulp.src("build")
+  .pipe(ws({livereload:true, open:true}));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
+};
 
 const img = () => 
   gulp .src(routes.img.src)
   .pipe(image())
   .pipe(gulp.dest(routes.img.dest));
 
+const styles = () =>
+  gulp
+  .src(routes.scss.src)
+  .pipe(sass().on("error", sass.logError))
+  .pipe(
+    autop({
+      browsers: ["last 2 versions"]
+    }))
+  .pipe(gulp.dest(routes.scss.dest));
+
 const prepare = gulp.series([clean, img]);
 
-const assets  = gulp.series([pug]);
+const assets  = gulp.series([pug, styles]);
 
-const postDev = gulp.series([webserver]);
+const postDev = gulp.parallel([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, postDev]);
 
