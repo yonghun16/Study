8881ba54ea8a54a6375d251807fdd20974f32179
yonghun16 <yonghun16@naver.com>
2023-08-10 23:08:44 +0900

    Update main.py

--- FrontEnd/Nomad_coders/job_scrapper/main.py
+++ FrontEnd/Nomad_coders/job_scrapper/main.py
@@ -1,17 +1,19 @@
-# from requests import get
-# from bs4 import BeautifulSoup
+from requests import get
+from bs4 import BeautifulSoup
 
-# base_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
-# search_term = "python"
+base_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
+search_term = "python"
 
-# response = get(f"{base_url}{search_term}")
-# if response.status_code != 200:
-#     print("Cant' request website")
-# else :
-#     soup = BeautifulSoup(response.text, "html.parser")
-#     jobs = soup.find_all('section', class_="jobs")
-
-def say_hello(name, age):
-    print(f"Hello {name} you are {age} years old")
-    
-say_hello("nico", 12)
+response = get(f"{base_url}{search_term}")
+if response.status_code != 200:
+    print("Cant' request website")
+else :
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all('section', class_="jobs")
+    for job_section in jobs:
+        job_posts = job_section.find_all('li')
+        job_posts.pop(-1)
+        for post in job_posts:
+            anchors = post.find_all('a')
+            anchor = anchors[1]
+            print(anchor['href'])
