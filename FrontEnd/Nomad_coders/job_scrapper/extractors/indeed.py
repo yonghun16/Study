e07c0e99c9e3379811415cf3f7a81f8fc7e1b81b
yonghun16 <yonghun16@naver.com>
2023-08-13 01:38:12 +0900

    update

---
+++ FrontEnd/Nomad_coders/job_scrapper/extractors/indeed.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+def get_page_count(keyword):
+    options =  Options()
+    browser = webdriver.Chrome(options=options)
+    base_url = "https://kr.indeed.com/jobs?q="
+    browser.get(f"{base_url}{keyword}")
+
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    pagination = soup.find("nav", role="navigation")
+    pages = pagination.find_all("div", recursive=False)
+    count = len(pages)
+    if count >= 5:
+        return 5
+    elif count == 0:
+        return 1
+    else:
+        return count 
+
+
+def extract_indeed_jobs(keyword):
+    pages = get_page_count(keyword)
+    results = []
+    for page in range(pages):
+        options =  Options()
+        browser = webdriver.Chrome(options=options)
+        base_url = "https://kr.indeed.com/jobs"
+        browser.get(f"{base_url}?q={keyword}&start={page*10}")
+
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        job_list = soup.find("ul", class_="jobsearch-ResultsList")
+        jobs = job_list.find_all('li', recursive=False)
+
+        for job in jobs:
+            zone = job.find("div", class_="mosaic-zone")
+            if zone == None:
+                anchor = job.select_one("h2 a")
+                title = anchor['aria-label']
+                link = anchor['href']
+                company = job.find("span", class_="companyName")
+                location = job.find("div", class_="companyLocation")
+                job_data = {
+                    'link' : f"https://kr.indeed.com{link}",
+                    'company' : company.string,
+                    'location' : location.string,
+                    'position' : title
+                }
+                results.append(job_data)
+    return results
--- FrontEnd/Nomad_coders/job_scrapper/main.py
+++ FrontEnd/Nomad_coders/job_scrapper/main.py
@@ -1,49 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
+from extractors.indeed import extract_indeed_jobs
 
-def get_page_count(keyword):
-    options =  Options()
-    browser = webdriver.Chrome(options=options)
-    base_url = "https://kr.indeed.com/jobs?q="
-    browser.get(f"{base_url}{keyword}")
+keyword = input("what do you want to search for? ")
 
-    soup = BeautifulSoup(browser.page_source, "html.parser")
-    pagination = soup.find("nav", role="navigation")
-    if pagination == None:
-        return 1
-    pages = pagination.find_all("div", recursive=False)
-    print(len(pages))
+indeed = extract_indeed_jobs(keyword)
+wwr = extract_wwr_jobs(keyword)
 
-get_page_count("python")
+jobs = indeed + wwr
 
+for job in jobs:
+    print(job)
+    print("/////// \n")
 
-def extract_indeed_jobs(keyword):
-    options =  Options()
-    browser = webdriver.Chrome(options=options)
-    base_url = "https://kr.indeed.com/jobs?q="
-    browser.get(f"{base_url}{keyword}")
-
-    results = []
-    soup = BeautifulSoup(browser.page_source, "html.parser")
-    job_list = soup.find("ul", class_="jobsearch-ResultsList")
-    jobs = job_list.find_all('li', recursive=False)
-
-    for job in jobs:
-        zone = job.find("div", class_="mosaic-zone")
-        if zone == None:
-            anchor = job.select_one("h2 a")
-            title = anchor['aria-label']
-            link = anchor['href']
-            company = job.find("span", class_="companyName")
-            location = job.find("div", class_="companyLocation")
-            job_data = {
-                'link' : f"https://kr.indeed.com{link}",
-                'company' : company.string,
-                'location' : location.string,
-                'position' : title
-            }
-            results.append(job_data)
-    for result in results:
-        print(result, "\n///////\n")
