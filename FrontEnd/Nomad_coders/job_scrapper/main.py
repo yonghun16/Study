2187752553aadc33de5f2093e660dca3c72d12ff
yonghun16 <yonghun16@naver.com>
2023-08-12 21:58:20 +0900

    study up

--- FrontEnd/Nomad_coders/job_scrapper/main.py
+++ FrontEnd/Nomad_coders/job_scrapper/main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-from extractors.wwr import extract_wwr_jobs
+# from extractors.wwr import extract_wwr_jobs
 
 options =  Options()
 options.add_argument("--no-sandbox")
@@ -12,10 +12,25 @@ base_url = "https://kr.indeed.com/jobs?q="
 search_term = "python"
 browser.get(f"{base_url}{search_term}")
 
+results = []
 soup = BeautifulSoup(browser.page_source, "html.parser")
 job_list = soup.find("ul", class_="jobsearch-ResultsList")
 jobs = job_list.find_all('li', recursive=False)
 print(len(jobs))
 for job in jobs:
     zone = job.find("div", class_="mosaic-zone")
-
+    if zone == None:
+        anchor = job.select_one("h2 a")
+        title = anchor['aria-label']
+        link = anchor['href']
+        company = job.find("span", class_="companyName")
+        location = job.find("div", class_="companyLocation")
+        job_data = {
+            'link' : f"https://kr.indeed.com{link}",
+            'company' : company.string,
+            'location' : location.string,
+            'position' : title
+        }
+        results.append(job_data)
+for result in results:
+    print(result, "\n///////\n")
