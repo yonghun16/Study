d858e9c51f1072c2debaaac61fffc73e6b9df6c2
yonghun16 <yonghun16@naver.com>
2023-10-06 22:51:36 +0900

    study up

---
+++ FrontEnd/Nomad_coders/js-examples/js/the_document_object.js
@@ -0,0 +1,10 @@
+const hello = document.getElementsByTagName("h1");
+
+console.log(document);
+// document는 오브젝트
+console.log(document.title);
+document.title = "test";
+console.log(document.title);
+
+console.log(hello);
+console.log(hello.length);
