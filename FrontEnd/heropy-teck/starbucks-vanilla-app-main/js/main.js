1c19ab140099f1a1fc552195540ad79e5d979b55
yonghun16 <yonghun16@naver.com>
2023-11-16 23:13:07 +0900

    업데이트

--- FrontEnd/heropy-teck/starbucks-vanilla-app-main/js/main.js
+++ FrontEnd/heropy-teck/starbucks-vanilla-app-main/js/main.js
@@ -60,8 +60,18 @@ fadeEls.forEach(function (fadeEl, index) {
 
 /* Swiper 이펙트 */
 // new Swiper(선택자, 옵션) 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.notice-line .swiper', {
   direction: 'vertical',
   autoplay: true,
   loop: true
 });
+
+const swiper2 = new Swiper('.promotion .swiper', {
+  slidesPerView: 3,  // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10,  // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가우데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000     // 5초에 한 번씩 슬라이딩
+  },
+})
