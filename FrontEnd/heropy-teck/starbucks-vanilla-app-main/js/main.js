a9f3adeb40743333fbbe68d5385bbc0ed7a6dca1
yonghun16 <yonghun16@naver.com>
2023-11-19 13:06:49 +0900

    이미지 슬라이드

--- FrontEnd/heropy-teck/starbucks-vanilla-app-main/js/main.js
+++ FrontEnd/heropy-teck/starbucks-vanilla-app-main/js/main.js
@@ -69,9 +69,17 @@ const swiper = new Swiper('.notice-line .swiper', {
 const swiper2 = new Swiper('.promotion .swiper', {
   slidesPerView: 3,  // 한번에 보여줄 슬라이드 개수
   spaceBetween: 10,  // 슬라이드 사이 여백
-  centeredSlides: true, // 1번 슬라이드가 가우데 보이기
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   loop: true,
-  autoplay: {
-    delay: 5000     // 5초에 한 번씩 슬라이딩
+  // autoplay: {
+  //   delay: 5000     // 5초에 한 번씩 슬라이딩
+  // },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자 
+    clickable: true // 사용자의 페이지 번호 요소 제어
   },
-})
+  navigation: {
+    prevEl: 'promotion .swiper-prev',
+    nextEl: 'promotion .swiper-next'
+  }
+});
