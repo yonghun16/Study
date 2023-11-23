fafac6e6b60789ab0101cbfcee247ca34492f870
yonghun16 <yonghun16@naver.com>
2023-11-24 00:27:18 +0900

    update

--- FrontEnd/JS_algorithm/01/fs_input.js
+++ FrontEnd/JS_algorithm/01/fs_input.js
@@ -1,8 +1,8 @@
 // fs를 이용해 파일 전체를 읽어 들여 처리하기
 
-let fs = require('fs');
-let inputData = fs.readFileSync('/dev/stdin').toString().split('\n');
-let inputVal = inputData[0].split(' ').map(Number);
+const fs = require('fs');
+const inputData = fs.readFileSync('/dev/stdin').toString().split('\n');
+const inputVal = inputData[0].split(' ').map(Number);
 
 main(inputVal);
 
