9e351aa84e084e2ad4481026a9ba966930bf8fd2
yonghun16 <yonghun16@naver.com>
2023-11-23 22:59:17 +0900

    Update max.js

--- BackEnd/10-week_algorithm_coding_test/01/1A/max.js
+++ BackEnd/10-week_algorithm_coding_test/01/1A/max.js
@@ -1,13 +1,13 @@
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 const inputData = [];
-rl.on("line", function(line) {
+rl.on("line", function (line) {
   inputData.push(line.trim());
-}).on("close", function() {
+}).on("close", function () {
   const inputVal = inputData[0].split(" ").map(Number);
   main(inputVal);
   process.exit();
@@ -15,14 +15,12 @@ rl.on("line", function(line) {
 
 function main(inputVal) {
   const a = inputVal[0];
-    const b = inputVal[1];
+  const b = inputVal[1];
 
-  console.log(getMax(a,b));
+  console.log(getMax(a, b));
 }
 
 function getMax(a, b) {
-  if ( a >= b)
-    return a;
-  else
-    return b;
+  if (a >= b) return a;
+  else return b;
 }
