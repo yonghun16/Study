18174f63df8040819cce177c41b5a89714eb71bf
yonghun16 <yonghun16@naver.com>
2023-11-24 10:44:28 +0000

    업데이트

---
+++ Algorithm/Jungol/j5931/j5931.py
@@ -0,0 +1,9 @@
+# 숫자사각형 4-1
+# https://www.jungol.co.kr/problem/5931
+
+inputVal_a = int(input())
+
+for i in range(0, inputVal_a):
+    for j in range(0, inputVal_a):
+        print(i + 1, end=' ')
+    print()
