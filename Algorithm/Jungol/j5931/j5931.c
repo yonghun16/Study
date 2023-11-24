9a89aad031dfdd96e67b20a8aa50bb23ff860818
yonghun16 <yonghun16@naver.com>
2023-11-24 09:07:50 +0000

    update

--- Algorithm/Jungol/j5931/j5931.c
+++ Algorithm/Jungol/j5931/j5931.c
@@ -2,37 +2,17 @@
 // https://www.jungol.co.kr/problem/5931
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int* getQuadrangle(int* arr, int inputVal_a);
-int answer(int *arr, int inputval_a);
+int main(void) {
+    int inputVal_a;
+    scanf("%d", &inputVal_a);
 
-int main() {
-  int inputVal_a;
-  scanf("%d", &inputVal_a);
+    for(int i=0; i<inputVal_a; i++) {
+        for(int j=0; j<inputVal_a; j++) {
+            printf("%d ", i+1);
+        }
+        printf("\n");
+    }
 
-  int* arr = (int *)malloc(sizeof(int) * inputVal_a * inputVal_a);   // 동적할당
-
-  answer(getQuadrangle(arr, inputVal_a), inputVal_a);  // 답변 출력
-
-  free(arr);
-  return 0;
-}
-
-int* getQuadrangle(int *arr, int inputVal_a) {
-  int num = 1;
-
-  for (int i = 0; i < inputVal_a * inputVal_a; i++) {
-    arr[i] = (i + 1) % inputVal_a == 0 ? num++ : num;
-  }
-
-  return arr;
-}
-
-int answer(int* arr, int inputVal_a) {
-  for (int i = 0; i < inputVal_a * inputVal_a; i++) {
-    if (i != 0 && i % inputVal_a == 0)
-      printf("\n");
-    printf("%d ", arr[i]);
-  }
+    return 0;
 }
