666f3ebfe0646331cffe97e2d62e4b97f5452d62
yonghun16 <yonghun16@naver.com>
2024-02-03 14:14:52 +0900

    b8339

--- Algorithm/Backjun/b8393/b8393.js
+++ Algorithm/Backjun/b8393/b8393.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 const inputData = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 let n = Number(inputData[0]);
-let summary = 0;
+let sum = 0;
 
 for (let i=1; i<=n; i++) {
-  summary += i;
+  sum += i;
 }
 
-console.log(summary);
+console.log(sum);
