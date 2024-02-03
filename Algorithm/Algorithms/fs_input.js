8ed5a8706cf05094d011060116bd2a523d0e9727
yonghun16 <yonghun16@naver.com>
2024-02-04 02:45:01 +0900

    upload

--- Algorithm/Algorithms/fs_input.js
+++ Algorithm/Algorithms/fs_input.js
@@ -1,4 +1,6 @@
-// fs를 이용해 파일 전체를 읽어 들여 처리하기
+/* 
+ * fs를 이용해 파일 전체를 읽어 들여 처리하기
+ */
 
 const fs = require('fs');
 const inputData = fs.readFileSync('/dev/stdin').toString().split('\n');
--- Algorithm/Algorithms/readline_input.js
+++ Algorithm/Algorithms/readline_input.js
@@ -1,4 +1,6 @@
-// 한 줄식 입력을 받아서, 처리하여 정답을 출력할 때는 readline 모듈을 사용할 수 있다.
+/* 
+ * 한 줄씩 입력을 연달아 받아야 할 때는 readline 모듈을 사용.
+ */
 
 const rl = require("readline").createInterface({
   input: process.stdin,
@@ -6,13 +8,15 @@ const rl = require("readline").createInterface({
 });
 
 const inputData = [];
+
 rl.on("line", function (line) {
-  // 콘솔 입력 창에서 줄바꿈(Enter)를 입려할 때마다 호출
+  // 콘솔 입력 창에서 줄바꿈(Enter)를 입력할 때마다 호출
   inputData.push(line.trim());
 }).on("close", function () {
-  // 콘솔 입력 창에서 Ctrl+C 혹은 Ctrl+D를 입력하면 호출(입력의 종료)
   const inputVal = inputData[0].split(" ").map(Number);
+
   main(inputVal);
+
   process.exit();
 });
 
