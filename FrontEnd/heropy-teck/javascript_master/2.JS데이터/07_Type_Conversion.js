1206741dece42d0917ea9b779ae1e6f22e3bd286
yonghun16 <yonghun16@naver.com>
2024-02-17 14:43:54 +0900

    Update 07_Type_Conversion.js

---
+++ FrontEnd/heropy-teck/javascript_master/2.JS데이터/07_Type_Conversion.js
@@ -0,0 +1,12 @@
+/* 형 변환 */
+
+const a = 1   // Number
+const b = '1' // String
+
+console.log(a === b)   // 형까지 동등한지 확인
+console.log(a == b)    // 값만 동등 한지 확인
+
+
+const c = true
+const d = 1
+console.log(c == d)    // 값만 동등 한지 확인
