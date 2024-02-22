c01168a5b9855f79841ef497d914013a94239223
yonghun16 <yonghun16@naver.com>
2024-02-23 02:33:12 +0900

    update

--- FrontEnd/heropy-teck/javascript_master/3.산술_연산자/03_논리_연산자.js
+++ FrontEnd/heropy-teck/javascript_master/3.산술_연산자/03_논리_연산자.js
@@ -10,12 +10,18 @@ console.log(1 && 2 && 3)        // 3
 console.log(3 && 2 && 1)        // 1 -> 0(거짓)이 나오지 않으니 1까지 확인한 뒤 1 출력
 console.log(0 && 2 && 1)        // 0 -> 0부터 거짓이니 0을 출력
 console.log('A' && 'B' && '')   // ''
-console.log('A' && 'B' && 'C')  // C
+console.log('A' && 'B' && 'C')  // C 거짓을 만나지 못하면 마지막에 있는 값을 반환함
 console.log()
 
 // Logical OR (||)
-console.log(true || true);   // true
-console.log(true || false);  // true
+console.log(true || true);           // true
+console.log(true || false);          // true
+console.log(0 || 1);                 // 1
+console.log(false || 0 || {});       // {}    -> true
+console.log(false || [] || null);    // []    -> 참을 만날 때까지 다음 항을 확인
+console.log(false || 0 || null);     // null  -> 참을 만날 때까지 다음 항을 확인
+console.log(false || null || 1);     // 1     -> 참을 만날 때까지 다음 항을 확인
+console.log()
 
 // Logical NOT (!)
 console.log(!true);  // false
---
+++ FrontEnd/heropy-teck/javascript_master/3.산술_연산자/04_Nullish병합_삼항연산자.js
@@ -0,0 +1,7 @@
+/* Nullish 병합(Nullish Coalescing)*/
+
+const a = 0;
+
+const num1 = n || 7
+
+console.log(num1)
