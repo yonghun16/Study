70fe2e3e779abd6868bb4f5db08572022b5f10be
yonghun16 <yonghun16@naver.com>
2024-02-27 03:12:59 +0900

    update

--- FrontEnd/heropy-teck/javascript_master/3.산술_연산자/04_Nullish병합_삼항연산자.js
+++ FrontEnd/heropy-teck/javascript_master/3.산술_연산자/04_Nullish병합_삼항연산자.js
@@ -1,7 +1,11 @@
 /* Nullish 병합(Nullish Coalescing)*/
 
-const a = 0;
+const n = 0;
 
+// OR 연산자를 사용한 경우
 const num1 = n || 7
+console.log(num1)   // 7
 
-console.log(num1)
+// Nullish 병합 연산자를 사용한 경우
+const num2 = n ?? 7
+console.log(num2)   // 0
