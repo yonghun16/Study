5e5f4a124e015aa9445302246c70fdca23eea26c
yonghun16 <yonghun16@naver.com>
2024-02-29 19:30:24 +0900

    Update 04_Nullish병합_삼항연산자.js

--- FrontEnd/heropy-teck/javascript_master/3.산술_연산자/04_Nullish병합_삼항연산자.js
+++ FrontEnd/heropy-teck/javascript_master/3.산술_연산자/04_Nullish병합_삼항연산자.js
@@ -2,10 +2,44 @@
 
 const n = 0;
 
-// OR 연산자를 사용한 경우
-const num1 = n || 7
+// OR (||) 연산자를 사용한 경우
+const num1 = n || 9 || 8
 console.log(num1)   // 7
 
-// Nullish 병합 연산자를 사용한 경우
+// Nullish (??) 병합 연산자를 사용한 경우
 const num2 = n ?? 7
 console.log(num2)   // 0
+console.log(null ?? 1)  // 1
+console.log(undefined ?? 2)  // 
+console.log(null ?? undefined)  // undefined
+console.log(null ?? 1 ?? 2)  //1
+console.log(false ?? 1 ?? 2)  // false
+console.log(0 ?? 1 ?? 2)  // 0
+
+
+console.log()
+console.log("--------------")
+console.log()
+
+
+// 삼항(Ternary)
+// 조건 ? 참 : 거짓
+const a = 1
+
+if (a < 2) {
+  console.log('참!')
+} else {
+  console.log('거짓...')
+}
+
+console.log(a < 2  ? '참!' : '거짓...')
+
+
+function getAlert(message) {
+  return message
+    ? message
+    : 'no message'
+}
+
+console.log(getAlert('안녕하세요'))
+console.log(getAlert(''))
