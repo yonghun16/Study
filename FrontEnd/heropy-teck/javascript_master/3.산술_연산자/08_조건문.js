aa629fa2b538793c74785f87b3833349d2919eef
yonghun16 <yonghun16@naver.com>
2024-03-06 20:57:06 +0900

    Create 08_조건문.js

---
+++ FrontEnd/heropy-teck/javascript_master/3.산술_연산자/08_조건문.js
@@ -0,0 +1,22 @@
+/* If */
+
+function isPositive(number) {
+  if (number > 0) {
+    return '양수'
+  } else if (number < 0){
+    return '음수'
+  } else {
+    return '0'
+  }
+}
+
+// Switch 
+
+switch (number) {
+  case 1:
+    return '양수'
+  case -1:
+    return '음수'
+  default:
+    return '0'
+}
