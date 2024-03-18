96d7d60df3ac103c355efaa1af111bb2e74eadd8
yonghun16 <yonghun16@naver.com>
2024-03-18 22:49:39 +0900

    Update 04.화살표_함수.js

--- FrontEnd/heropy-teck/javascript_master/4.함수/04.화살표_함수.js
+++ FrontEnd/heropy-teck/javascript_master/4.함수/04.화살표_함수.js
@@ -3,19 +3,16 @@
 // function sum() {}
 // const sum = function () {}
 // const sum = () => {}
-//
+
 // function sum(a, b) {
 //   return a + b
 // }
-
 // const sum = (a, b) => {
 //   return a + b
 // }
 // const sum = (a, b) => a + b 
-//
-// console.log(sum(1, 2))     // 2
-// console.log(sum(10, 20))  // 20
 
+// 화살표 함수의 다양한 패턴
 const a = () => {}
 const b = x => {}       // 매개변수가 1개 일 때
 const c = (x, y) => {}  // 매개변수가 2개 일 때
@@ -26,4 +23,6 @@ const f = x => {
   return x * x
 }
 const g = () => { return { a: 1} }
-const h = () => ({ a: 1 })
+const h = () => ({ a: 1 })   // 객체 데이터는 소괄호로 묶어준다.
+const i = () => { return [1, 2, 3] }
+const j = () => [1, 2, 3]    // 배열 데이터는 배열 그대로 써준다.
