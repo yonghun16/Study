1198092c226a1d625e5332d4066db91644618bb4
yonghun16 <yonghun16@naver.com>
2024-03-19 20:34:27 +0900

    Create 05.즉시실행함수.js

---
+++ FrontEnd/heropy-teck/javascript_master/4.함수/05.즉시실행함수.js
@@ -0,0 +1,32 @@
+/* 즉시실행함수 (IIFE, Immediately Invoked Function Expression) */
+
+const a = 7
+
+const double = () => {
+  console.log(a * 2)
+}
+double()
+
+;(() => {
+  console.log(a * 2)
+})()
+
+;(() => {})()         // (F)()
+;(function () {})()   // (F)()
+;(function () {}())   // (F())
+;!function () {}()    // !F()
+;+function () {}()    // +F()
+
+
+;(() => { console.log(a * 2) })()         // (F)()
+;(function () { console.log(a * 2) })()   // (F)()
+;(function () { console.log(a * 2) }())   // (F())
+;!function () { console.log(a * 2) }()    // !F()
+;+function () { console.log(a * 2) }()    // +F()
+
+
+// 심화 (매개 변수 넣기)
+;((a, b) => {
+  console.log(a.innerWidth)
+  console.log(b.body)
+})(window, document)    // 외부의 변수를 내부의 변수의 이름으로 사용 가능
