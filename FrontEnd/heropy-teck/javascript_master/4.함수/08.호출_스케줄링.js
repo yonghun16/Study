a1ec8749f80bff0c36a306f90a603b3cad82520c
yonghun16 <yonghun16@naver.com>
2024-03-23 17:53:11 +0900

    Create 08.호출_스케줄링.js

---
+++ FrontEnd/heropy-teck/javascript_master/4.함수/08.호출_스케줄링.js
@@ -0,0 +1,12 @@
+/* 호출 스케쥴링 (scheduling a function call) */
+
+const hello = () => {
+  console.log("hello~")
+}
+
+const timeout = setInterval(hello, 1000)
+const h1El = document.querySelector('h1')
+h1El.addEventListener('click', () => {
+  console.log('Clear!')
+  clearInterval(timeout)
+})
