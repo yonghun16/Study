fac51223feb1a436f396a284990b2467ba5cb2c8
yonghun16 <yonghun16@naver.com>
2024-03-30 19:33:47 +0900

    Update 03.Getter_Setter.js

--- FrontEnd/heropy-teck/javascript_master/5.클래스/03.Getter_Setter.js
+++ FrontEnd/heropy-teck/javascript_master/5.클래스/03.Getter_Setter.js
@@ -1,1 +1,22 @@
 // Getter, Setter
+
+class User {
+  constructor(first, last) {
+    this.firstName = first
+    this.lastName = last
+  }
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+  set fullName(value) {
+    ;[this.firstName, this.lastName] = value.split(' ')
+  }
+}
+
+const heopy = new User('Heropy', 'Park')   // 초기값
+
+console.log(heopy.fullName)        // getter를 통해 값을 불러옴
+
+heopy.fullName = 'Jung ManHo'      // setter를 통해 값을 설정
+
+console.log(heopy.fullName)
