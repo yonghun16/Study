6b2095acb4ebd14091681b43884db5288f3bb1e5
Frontend16 <yonghun16@naver.com>
2024-04-05 02:15:02 +0000

    upload

---
+++ BackEnd/모두의_파이썬/Day 10/10-3.py
@@ -0,0 +1,17 @@
+# 10-3
+# 숫자를 추측해서 맞히는 프로그램
+
+import random
+
+n = random.randint(1, 30)
+
+while True:
+    x = input("맞혀 보세요? ")
+    g = int(x)
+    if g == n:
+        print("정답")
+        break
+    if g < n:
+        print("너무 작아요.")
+    if g > n:
+        print("너무 커요.")
