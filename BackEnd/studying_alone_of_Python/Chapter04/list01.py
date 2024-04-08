5fa9289c04916433422f1ea0c39ce4af24da41ed
yonghun16 <yonghun16@naver.com>
2024-04-08 12:45:11 +0900

    update

--- BackEnd/studying_alone_of_Python/Chapter04/list01.py
+++ BackEnd/studying_alone_of_Python/Chapter04/list01.py
@@ -10,10 +10,10 @@ print()
 
 #기본 연산자
 print("# 리스트 기본 연산자")
-print("list_a + list_b =", list_a + list_b)
-print("list_a * 3 =", list_a * 3)
+print("list_a + list_b =", list_a + list_b)       # list_a + list_b = [1, 2, 3, 4, 5, 6]
+print("list_a * 3 =", list_a * 3)                 # list_a * 3 = [1, 2, 3, 1, 2, 3, 1, 2, 3]
 print()
 
 #함수
 print("# 길이 구하기")
-print("len(list_a) =", len(list_a))
+print("len(list_a) =", len(list_a))               # len(list_a) = 3
--- BackEnd/studying_alone_of_Python/Chapter04/list02.py
+++ BackEnd/studying_alone_of_Python/Chapter04/list02.py
@@ -5,10 +5,10 @@ list_a = [1, 2, 3]
 print("#리스트 뒤에 요소 추가하기")
 list_a.append(4)
 list_a.append(5)
-print(list_a)
+print(list_a)                       # [1, 2, 3, 4, 5]
 print()
 
 #리스트 중간에 요소 추가하기
 print("#리스트 중간에 요소 추가하기")
 list_a.insert(1, 10)
-print(list_a)
+print(list_a)                       # [1, 10, 2, 3, 4, 5]
