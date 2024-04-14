c632ac6c8cbdfde8786b876477a5800a43e2518a
yonghun16 <yonghun16@naver.com>
2024-04-14 14:04:02 +0900

    Update 05_Object.js

--- FrontEnd/heropy-teck/javascript_master/2.JS데이터/05_Object.js
+++ FrontEnd/heropy-teck/javascript_master/2.JS데이터/05_Object.js
@@ -1,26 +1,27 @@
 /* Object(객체) */
 
-// const user = new Object();   // 생성자 함수 방식으로 객체 데이터 생성
-// user.name = 'Heropy';
-// user.age = 85;
+const user1 = new Object();     // 생성자 함수 방식으로 객체 데이터 생성
+user1.name = 'Heropy';
+user1.age = 85;
 
-// function User() {       // 함수 내부에서 객체 데이터 생성
-//   this.name = 'Heropy'
-//   this.age = 85
-// }
-// const user = new User()
+function User2() {              // 함수 내부에서 객체 데이터 생성
+  this.name = 'Heropy'
+  this.age = 85
+}
 
-// const user = {            // 리터럴 방식으로 객체 데이터 생성
-//   name: 'Heropy',
-//   age: 85
-// }
-// const keyName = 'name'
-// console.log(user.name)     // 점표(.) 표기법
-// console.log(user['name'])  // 대괄호 표기법
-// console.log(user[keyName])  // 대괄호 표기법
+const user2 = new User2()
+const user3 = {                 // 리터럴 방식으로 객체 데이터 생성
+  name: 'Heropy',
+  age: 85
+}
+
+const keyName = 'name'
+console.log(user1.name)         // 점표(.) 표기법
+console.log(user2['name'] )     // 대괄호 표기법
+console.log(user3[keyName])     // 대괄호 표기법
 
 
-
+// 표기법의 활용
 const userA = {
   name: 'Heropy',
   age: 85
@@ -30,8 +31,8 @@ const userB = {
   age: 55,
   parent: userA
 }
-console.log(userB.parent.name)
-console.log(userB['parent']['name'])
+console.log(userB.parent.name)        // userA.name가 출력 되므로 heropy
+console.log(userB['parent']['name'])  // heropy
 
 const users = [userA, userB]
-console.log(users[0].name)
+console.log(users[0].name)            // heropy
