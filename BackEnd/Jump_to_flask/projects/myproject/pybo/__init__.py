ebb377fb40b5ba23d506d8d7b0a42b09ba9cf5d8
yonghun16 <yonghun16@naver.com>
2024-05-12 23:02:04 +0900

    2-04 도델로 데이터 처리하기

--- BackEnd/Jump_to_flask/projects/myproject/pybo/__init__.py
+++ BackEnd/Jump_to_flask/projects/myproject/pybo/__init__.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask_migrate import Migrate          # flask의 python모델을 이용해 테이블 생성, 컬럼 추가 할 수 있게 하는 기능을 가진 라이브러리 
+from flask_sqlalchemy import SQLAlchemy    # flask의 ORM 라이브러리
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():                           # Flask app생성 함수 (애플리케이션 펙토리)
     app = Flask(__name__)                   # app 객체 생성 (플라스크 app으로) 
+    app.config.from_object(config)          # config.py를 불러옴
 
+    # ORM
+    db.init_app(app)                        # ORM객체 초기화 및 등록
+    migrate.init_app(app, db)               # migrate객체 초기화 및 등록
+    from . import models
+
+    # 블루프린트
     from .views import main_views           # main_views를 불러옴 (화면을 구성하는 메인뷰 파일)
-    app.register_blueprint(main_views.bp)   # main_views를 블루프린트에 등록함.
+    app.register_blueprint(main_views.bp)   # main_views에 작성한 블루프린트를 등록함.
 
     return app                              # app 객체 반환
 
---
+++ BackEnd/Jump_to_flask/projects/myproject/pybo/models.py
@@ -0,0 +1,18 @@
+from pybo import db     # pybo(__init__.py)의 db를 불러옴
+
+# 질문 모델 클래스
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)              # id : 답변 데이터의 고유번호
+    subject = db.Column(db.String(200), nullable=False)       # subject : 질문 제목
+    content = db.Column(db.Text(), nullable=False)            # content : 질문 내용
+    create_date = db.Column(db.DateTime(), nullable=False)    # create_date : 질문 작성일시
+
+
+# 답변 모델 클래스
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)                                            # id : 답변 데이터의 고유 번호
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))   # question_id : 질문의 데이터의 고유번호(어떤 질문에 달린 답변인지 알아야 하므로 질문 데이터 번호가 필요함)
+    question = db.relationship('Question', backref=db.backref('answer_set'))                # question : 질문에 대한 역참조
+    content = db.Column(db.Text(), nullable=False)                                          # content : 답변 내용 
+    create_date = db.Column(db.DateTime(), nullable=False)                                  # create_date : 답변 작성일시
+
