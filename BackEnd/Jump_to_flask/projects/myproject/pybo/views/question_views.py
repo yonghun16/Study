614c958e1a3e31934285f9e202c94f90947cd7c3
yonghun16 <yonghun16@naver.com>
2024-05-15 21:03:57 +0900

    2-10

--- BackEnd/Jump_to_flask/projects/myproject/pybo/views/question_views.py
+++ BackEnd/Jump_to_flask/projects/myproject/pybo/views/question_views.py
@@ -4,7 +4,7 @@ from werkzeug.utils import redirect
 
 from .. import db
 from ..models import Question
-from ..forms import QuestionForm
+from ..forms import QuestionForm, AnswerForm
 
 
 bp = Blueprint('question', __name__, url_prefix='/question')
@@ -18,15 +18,16 @@ def _list():
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question=question) 
+    return render_template('question/question_detail.html', question=question, form=form) 
 
 
 @bp.route('/create/', methods=('GET', 'POST'))
 def create():
     form = QuestionForm()
     if request.method == 'POST' and form.validate_on_submit():
-        question = Question(subject=form.subject.data, content=form.content.data, create_data=datetime.now())
+        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
         db.session.add(question)
         db.session.commit()
         return redirect(url_for('main.index'))
