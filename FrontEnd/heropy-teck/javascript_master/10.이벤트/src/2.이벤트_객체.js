056c18867faff23f0e5cc929bfac013a5b2ab8ee
yonghun16 <yonghun16@naver.com>
2024-07-07 00:51:18 +0900

    Create 2.이벤트_객체.js

---
+++ FrontEnd/heropy-teck/javascript_master/10.이벤트/src/2.이벤트_객체.js
@@ -0,0 +1,24 @@
+// 이벤트 객체
+
+// 이벤트 객체는 대상에서 발생한 이벤트 정보를 담고 있습니다.
+// 예를들어 마우스 이벤트 객체에는 마우의 이벤트 정보를 가지고 있습니다.
+// click, wheel 등
+
+const parentEl = document.querySelector('.parent')
+
+parentEl.addEventListener('click', event => {
+  console.log(event.target, event.currentTarget)
+  // event 목록을 볼 수 있다.
+  console.log(event)
+})
+// wheel를 돌릴 때마다 event콜백 함수를 실행한다.
+parentEl.addEventListener('wheel', event => {
+  console.log(event)
+})
+
+const inputEl = document.querySelector('input')
+
+// key를 누를 때 마다 event콜백 함수를 실행한다.
+inputEl.addEventListener('keydown', event => {
+  console.log(event.key)
+})
