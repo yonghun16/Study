23199b24ad2f4fc782c03fd5d18e91fcec07c6eb
yonghun16 <yonghun16@naver.com>
2024-07-15 01:05:49 +0900

    Update 1B.js

--- Algorithm/10_week_Algorithms/1.선형_알고리즘_기초/1B.js
+++ Algorithm/10_week_Algorithms/1.선형_알고리즘_기초/1B.js
@@ -5,8 +5,8 @@ const readline = require('readline')
 
 // 입력을 읽기 위한 인터페이스 설정
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 })
 
 // 입력을 저장할 변수
@@ -14,22 +14,22 @@ let input = []
 
 // 각 줄의 입력을 처리
 rl.on('line', (line) => {
-    input.push(line)
+  input.push(line)
 })
 
 // 모든 입력을 받은 후 처리
 rl.on('close', () => {
-    const n = parseInt(input[0].trim(), 10)
-    const inputNumbers = input[1].trim().split(' ').map(Number)
-    const answer = getSum(inputNumbers)
-    console.log(answer)
+  const n = parseInt(input[0].trim(), 10)
+  const inputNumbers = input[1].trim().split(' ').map(Number)
+  const answer = getSum(inputNumbers)
+  console.log(answer)
 })
 
 // 합계를 계산하는 함수
 function getSum(inputNumbers) {
-    let sum = 0
-    for (let num of inputNumbers) {
-        sum += num
-    }
-    return sum
+  let sum = 0
+  for (let num of inputNumbers) {
+    sum += num
+  }
+  return sum
 }
