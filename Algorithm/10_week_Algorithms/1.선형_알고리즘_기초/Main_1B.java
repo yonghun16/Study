c8518466da39a6e7e7f335a6b36375d1d457bbd8
yonghun16 <yonghun16@naver.com>
2024-07-15 01:26:47 +0900

    Update Main_1B.java

--- Algorithm/10_week_Algorithms/1.선형_알고리즘_기초/Main_1B.java
+++ Algorithm/10_week_Algorithms/1.선형_알고리즘_기초/Main_1B.java
@@ -1,10 +1,9 @@
 // 문제1B-원소의 합 구하기
 
-
 import java.util.Scanner;
 
 public class Main_1B {
-    
+
     // 입력된 정수 배열의 합을 계산하는 함수
     public static int getSum(int[] inputNumbers) {
         int sum = 0;
@@ -19,7 +18,7 @@ public class Main_1B {
 
         // 첫 번째 줄에서 데이터의 수 n을 읽습니다.
         int n = scanner.nextInt();
-        
+
         int[] inputNumbers = new int[n];
 
         // 두 번째 줄에서 공백으로 구분된 n개의 정수를 읽어옵니다.
@@ -28,12 +27,12 @@ public class Main_1B {
         }
 
         scanner.close();
-        
+
         // 합계를 계산합니다.
         int answer = getSum(inputNumbers);
-        
+
         // 결과를 출력합니다.
         System.out.println(answer);
-        
+
     }
 }
