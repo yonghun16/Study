a270e9c516b8ba7478366440ba7ff839e36c8145
yonghun16 <yonghun16@naver.com>
2024-07-14 15:59:04 +0900

    b2393

---
+++ Algorithm/Backjun/bronze5/b2393/b2393.java
@@ -0,0 +1,24 @@
+// Rook
+// https://www.acmicpc.net/problem/2393
+
+public class b2393 {
+    public static void printRook() {
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/");
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
+    }
+
+    public static void main(String[] args) {
+        printRook();
+    }
+}
---
+++ Algorithm/Backjun/bronze5/b2393/b2393.py
@@ -0,0 +1,19 @@
+# Rook
+# https://www.acmicpc.net/problem/2393
+
+def print_rook():
+    print("  ___  ___  ___")
+    print("  | |__| |__| |")
+    print("  |           |")
+    print("   \\_________/")
+    print("    \\_______/")
+    print("     |     |")
+    print("     |     |")
+    print("     |     |")
+    print("     |     |")
+    print("     |_____|")
+    print("  __/       \\__")
+    print(" /             \\")
+    print("/_______________\\")
+
+print_rook()
