1088a926bdcf4dc519daabdcd4e2b796a9b35cb4
yonghun16 <yonghun16@naver.com>
2024-09-07 22:03:59 +0900

    Update Main_1E.java

--- BackEnd/10_week_Algorithms/1.선형_알고리즘_기초/1E/Main_1E.java
+++ BackEnd/10_week_Algorithms/1.선형_알고리즘_기초/1E/Main_1E.java
@@ -1,29 +1,27 @@
-// 1E 합 구학기2
+// 1E 합 구하기2
 
 import java.io.*;
 import java.lang.*;
 import java.util.*;
 
-
 public class Main_1E {
-	public static Scanner scanner = new Scanner(System.in);
+  public static Scanner scanner = new Scanner(System.in);
 
-	/**
-     * 동아리 멤버들의 탑승 가능 여부를 출력하는 함수
-     *
-     * @param data 각 멤버들의 몸무게 data[0] ~ data[n-1]
-     * @param n    멤버들의 수
-     * @param p    놀이기구 탑승 가능 체중 제한
-     * @param q    놀이기구 최대 하중
-     */
-	public static void solve(int[] data, int n, int p, int q)
-	{
-		int S = 0;
-		int C = 0;
+  /**
+   * 동아리 멤버들의 탑승 가능 여부를 출력하는 함수
+   *
+   * @param data 각 멤버들의 몸무게 data[0] ~ data[n-1]
+   * @param n    멤버들의 수
+   * @param p    놀이기구 탑승 가능 체중 제한
+   * @param q    놀이기구 최대 하중
+   */
+  public static void solve(int[] data, int n, int p, int q) {
+    int S = 0;
+    int C = 0;
     boolean able = false;
 
-    for(int i =0; i < n; i+=1) {
-      if(data[i] <= p) {
+    for (int i = 0; i < n; i += 1) {
+      if (data[i] <= p) {
         S += data[i];
         C += 1;
       }
@@ -31,26 +29,24 @@ public class Main_1E {
 
     able = (S <= q);
 
-    if(able) {
+    if (able) {
       System.out.printf("%d %d\n", C, S);
       System.out.printf("YES");
-    }else {
+    } else {
       System.out.printf("%d %d\n", C, S);
       System.out.printf("NO");
     }
-	}
+  }
 
-	public static void main(String[] args)
-	{
-		int n = scanner.nextInt();
-		int p = scanner.nextInt();
-		int q = scanner.nextInt();
-		int[] data = new int[n];
-		for(int i = 0 ; i < n ; i ++)
-		{
-			data[i] = scanner.nextInt();
-		}
+  public static void main(String[] args) {
+    int n = scanner.nextInt();
+    int p = scanner.nextInt();
+    int q = scanner.nextInt();
+    int[] data = new int[n];
+    for (int i = 0; i < n; i++) {
+      data[i] = scanner.nextInt();
+    }
 
-		solve(data, n, p, q);
-	}
+    solve(data, n, p, q);
+  }
 }
