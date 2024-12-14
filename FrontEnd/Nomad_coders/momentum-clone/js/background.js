a029f332e8ca352d1a962a7d9f885bc1503336a6
yonghun16 <yonghun16@naver.com>
2024-12-14 14:21:31 +0900

    update

--- FrontEnd/Nomad_coders/momentum-clone/js/background.js
+++
@@ -1,9 +0,0 @@
-const images = [ "0.jpeg", "1.jpeg", "2.jpeg" ];
-
-const chosenImage = images[parseInt(Math.random()*images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
