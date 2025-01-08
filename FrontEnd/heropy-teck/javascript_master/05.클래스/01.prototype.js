1a4c122b15bcbf1b2f21ceb59d619858a1eb591b
yonghun16 <yonghun16@naver.com>
2025-01-08 17:34:54 +0900

    update prototype

--- FrontEnd/heropy-teck/javascript_master/05.클래스/01.prototype.js
+++ FrontEnd/heropy-teck/javascript_master/05.클래스/01.prototype.js
@@ -2,12 +2,12 @@
 
 const fruits = new Array('apple', 'mango', 'orange')
 
-console.log(fruits)
-console.log(fruits.length)   // 3
+console.log(fruits)                    // [ 'apple', 'mango', 'orange' ]
+console.log(fruits.length)             // 3
 console.log(fruits.includes('apple'))  // true
 
-Array.prototype.heropy = function () {
+Array.prototype.heropy = function () {  // Array의 프로토타입에 접근하여 함수 생성
   console.log(this)
 }
 
-fruits.heropy()
+fruits.heropy()         // [ 'apple', 'mango', 'orange' ]
