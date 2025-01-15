eae9251f3596ef66fbe4f8b730b4c9475f10cd06
yonghun16 <yonghun16@naver.com>
2025-01-15 19:57:30 +0900

    update

--- FrontEnd/Nomad_coders/typescript/src/exercise.ts
+++ FrontEnd/Nomad_coders/typescript/src/exercise.ts
@@ -1,1 +1,23 @@
-console.log("Console");
+abstract class Human {
+  constructor(
+    protected height: number,
+    protected weight: number
+  ){}
+
+  abstract bmi(): number;
+}
+
+class Mail extends Human {
+  sex: string
+
+  constructor(height: number, weight: number, sex: string) {
+    super(height, weight);
+    this.sex = sex
+  }
+  bmi() {
+    return this.weight / this.height**2;
+  }
+}
+
+const nico = new Mail(1.80, 70, "mail");
+console.log(nico.bmi())
