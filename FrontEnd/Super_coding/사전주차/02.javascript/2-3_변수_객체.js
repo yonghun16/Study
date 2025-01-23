612b85181d59003f4812e98000ca29affd280cba
yonghun16 <yonghun16@naver.com>
2025-01-24 04:51:05 +0900

    update

---
+++ FrontEnd/Super_coding/사전주차/02.javascript/2-3_변수_객체.js
@@ -0,0 +1,35 @@
+// string
+console.log("hello world!")
+console.log("123" + "345")
+
+// number
+console.log(123)
+console.log(123 + 345)
+
+// boolean
+console.log(true)
+console.log(false)
+
+// undefined
+// 정의가 안되어 있다.
+console.log(undefined)
+
+// null
+// 정의는 되어 있지만 비어있다.
+console.log(null)
+
+// array
+console.log([1, 2, 3, 4, 5])
+const buy = ["애플워치", "맥북", "아이패드", "애플펜슬"]
+console.log(buy[0])
+console.log(buy[1])
+console.log(buy[2])
+console.log(buy[3])
+
+// object
+const person = {
+  name: "song",
+  age: 26
+}
+console.log(person.name)
+console.log(person.age)
