0edd4d76cafd5b9aac3b45bb03fc152352b56d11
yonghun16 <yonghun16@naver.com>
2025-03-14 18:01:43 +0900

    복습

--- FrontEnd/Super_coding/javascript/13_this.js
+++ FrontEnd/Super_coding/javascript/13_this.js
@@ -12,27 +12,34 @@
    - 화살표 함수      : 둘러싸고 있는 함수 (부모 스코프의 this)
    - 이벤트 리스너    : 핸들러가 붙어 있는 DOM 이벤트
    - 생성자 함수(new) : 생성된 인스턴스
+
+ * 참고 url
+   - 함수 호출 방식에 의해 결정되는 this - https://poiemaweb.com/js-this
+   - [Javascript] this가 this가 아니라고?? = https://velog.io/@gidskql6671/Javascript-this
 -----------------------------------------------------*/
 
 // 1.
-console.log('1. ');
-console.log(this);                 // {} 빈 객체 출력
+console.log('\n--- 1. ');
+console.log("전역 컨텍스트의 this : ", this);    // {} 빈 객체 출력
+
 
 
 // 2.
 const calcAge = function(birthYear) {
-  console.log('2. ');
+  console.log('\n--- 2. ');
   console.log(2033 - birthYear);
-  console.log(this);               // 사용된 함수의 global object
+  console.log("함수 표현식에서의 this : ", this); // 사용된 함수의 object[global]
 }
 calcAge(1994);
 
 
 // 3.
 const calcAgeArrow = birthYear => {
-  console.log('3. ');
+  console.log('\n--- 3. ');
   console.log(2033 - birthYear);
-  console.log(this);               // 부모 함수의 global object
+  console.log("화살표함수의 this : ", this);      // {} ** 빈 객체 ** -> 부모 함수의 object[global]
+  // ↑ 호출된 객체가 아닌 외부의 this를 참조
+  // ** calcAgeArrow 함수 외부의 this인 global(node.js) 또는 window(브라우저)를 참조함
 }
 calcAgeArrow(1994);
 
@@ -42,35 +49,38 @@ const profile = {
   firstName: 'Kevin',
   year: 1992,
   calcAge: function() {
-    console.log(this)
-    return 2033 - this.year;       // 2033 - 1922(profile.year)
+    console.log("메서드에서의 this : ", this);    // { firstName: 'kevin', year: 1992, calcAge: [Function: calcAge] }
+    return 2033 - this.year;                      // 2033 - 1922(profile.year)
   },
   // 7.
-  greet: () => console.log(`안녕 ${this.firstName}`), // undefiend, parent scope는 global scope, global scope에서 this 키워드 사용.
+  greet: () => console.log(`안녕 ${this.firstName}`), // undefiend 출력, 
+                                                      // parent scope는 global scope임.  global scope에서 this 키워드 사용.
   // 8.
   greet2: function() { console.log(`안녕 ${this.firstName}`); }
 }
-console.log('4. ');
+console.log('\n--- 4. ');
 console.log(profile.calcAge());
-console.log('7. ');
+
+console.log('\n--- 7. ');
 profile.greet();
-console.log('8. ');
+
+console.log('\n--- 8. ');
 profile.greet2();
 
 
 // 5.
+console.log('\n--- 5. ');
 const anotherProfile = {
   firstName: 'Sara',
   year: 1986,
 }
-anotherProfile.calcAge = profile.calcAge;
-console.log('5. ');
-console.log(anotherProfile.calcAge());    // 2033 - 1986(anotherProfile.year)
+anotherProfile.calcAge = profile.calcAge;   //anotherProfile의 calcAge 메서드가 profile의 calcAge를 참조함
+console.log(anotherProfile.calcAge());      // 2033 - 1986(anotherProfile.year)
 
 
 // 6.
+console.log('\n--- 6. ');
 const foo = profile.calcAge;              
-console.log('6. ');
-console.log(foo());                       // foo는 함수고, profile는 객체다.
-                                          // profile.year를 참조 할 수 있는 property가 foo()에는 없기 때문에 
-                                          // NaN이 출력된다.
+console.log(foo());                         // foo는 함수고, profile는 객체다.
+                                            // profile.year를 참조 할 수 있는 property가 foo()에는 없기 때문에 
+                                            // NaN이 출력된다.
--- FrontEnd/Super_coding/javascript/15_datatype_object.js
+++ FrontEnd/Super_coding/javascript/15_datatype_object.js
@@ -1,11 +1,15 @@
 // Data Type - Object
+
 /* -------------------------------------------
- * Object, Array, Function
+ * 종류 :  Object, Array, Function
  *
+ * 얕은 복사 vs 깊은 복사 (shallow copy / deep copy)
+  - 얕은 복사 : 객체의 최상위 속성만 복사하고, 중첩된 객체는 참조를 공유하는 방식, 원본 객체와 중첩된 객체를 공유.
+  - 깊은 복사 : 객체의 모든 속성과 중첩된 객체까지 모두 복사하는 방식, 원본 객체와 복사된 객체가 서로 독립적으로 존재.
 ------------------------------------------- */
 
 // 1.
-console.log("1. ");
+console.log("\n--- 1. ");
 let myAddress = 'Seoul';
 let oldMyAddress = myAddress;
 myAddress = 'Busan';
@@ -21,7 +25,7 @@ console.log(afterProfile);
 
 
 // 2. 객체 복사하기
-console.log("2. ");
+console.log("\n--- 2. ");
 const profile2 = {
   address: 'Seoul',
   family: ['Tony', 'Chris']
@@ -30,13 +34,20 @@ const profile2 = {
 const profile2Copy = Object.assign({}, profile2);   // 얕은 복사. 프로퍼티가 원시 타입이면 복사 O, 객체 타입이면 X
 profile2Copy.address = 'Daegu';
 
-profile2Copy.family.push('Levin');       // profile2Copy.family배열에 넣지만, profile2.family배열도 값이 더해짐.
+profile2Copy.family.push('Levin');                  // profile2Copy.family 배열에 값을 넣지만, 같은 객체를 참조한다,
+                                                    // 때문에 profile2.family배열도 더해진 값이 조회가 됨.
 profile2Copy.family.push('Roy');
 
 console.log(profile2);
 console.log(profile2Copy);
 
 // deep clone은 lodash 사용 권장
+// - _.cloneDeep(target);
+// - target: 속성을 복사할 대상 객체.
+// - 터미널에서 프로젝트 라이브러리에 설치 : npm install lodash
+
+/*
+
 const _ = require("lodash");
 
 const obj1 = { name: "John", details: { age: 30 } };
@@ -44,3 +55,5 @@ const obj2 = _.cloneDeep(obj1); // 깊은 복사
 
 obj2.details.age = 40;
 console.log(obj1.details.age); // 30 (원본 영향 없음)
+
+*/
