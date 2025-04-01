2b8a44309e5caf6187f0770a8b21d937f39a6d86
yonghun16 <yonghun16@naver.com>
2025-04-02 04:34:45 +0900

    Update 24_function-reference.js

--- FrontEnd/Super_coding/javascript/24_function-reference.js
+++ FrontEnd/Super_coding/javascript/24_function-reference.js
@@ -29,8 +29,8 @@ function runFunction(fn) {
 runFunction(greet);  // 함수 참조를 전달
 
 
-/* 3. 함수 반환값으로 반환 */
-console.log('\n--- 3. 함수 반환값으로 반환 ');
+/* 3. 함수 반환값으로 반환 - 예시1*/
+console.log('\n--- 3. 함수 반환값으로 반환1');
 function greet() {
   console.log("Hello!");
 }
@@ -40,29 +40,35 @@ function getGreetingFunction() {
 }
 
 const greetings = getGreetingFunction();  // 함수 참조를 반환받음
+const greetings2 = getGreetingFunction;  // 함수 참조를 반환받음
 greetings();  // 함수 호출
 
+console.log(greetings);     // [Function: greet]   ->   getGreetingFunction()의 return값
+console.log(greetings2);    // [Function: getGreetingFunction]   ->   getGreetingFunction 자체
+console.log(greetings());   // greetings()가 실행되서 'Hello!' 출력 후 
+                            // greetings()의 반환값은 없으니 console.logt에 의해 'undefined'를 출력
 
-/* 4. 함수 참조와 함수 실행 */
-console.log('\n--- 4. 함수 참조와 함수 실행 ');
-function greeting(callback) {
-  return callback();
-}
 
+/* 4. 함수 반환값으로 반환 - 예시2 */
+console.log('\n--- 4. 함수 반환값으로 반환2');
 function afterGreeting() {
   console.log("Welcome");
 }
 
+function greeting(callback) {
+  return callback();
+}
+
 // 1) alert1는 '함수 참조'를 반환받아 실행 가능
-//const alert1 = greeting(() => afterGreeting);   //축약
+// const alert1 = greeting(() => afterGreeting);
 const alert1 = greeting(() => { return afterGreeting });  // 이 함수는 afterGreeting **함수 자체(참조)**를 반환하는 함수.
                                                           // 즉, callback()을 실행하면 afterGreeting 자체를 반환
                                                           // alert1 = return afterGreeting();
 alert1();                                                 // alert1() = afterGreeting();
 
-// 2) alert2는 undefined를 반환 받고 선언과 동시에 '함수 실행'
-//const alert2 = greeting(() => afterGreeting()); //축약
-//const alert2 = greeting(afterGreeting);         //또 축약
+// 2) alert2는 undefined를 반환 받고, 선언과 동시에 '함수 실행'
+// const alert2 = greeting(() => afterGreeting());
+// const alert2 = greeting(afterGreeting);
 const alert2 = greeting(() => { return afterGreeting()}); // greeting 내부에서 callback()이 실행되므로, 결국 afterGreeting()이 실행됨.
                                                           // (다시말해,  afterGreeting() 실행)
                                                           // alert2 = undefined
