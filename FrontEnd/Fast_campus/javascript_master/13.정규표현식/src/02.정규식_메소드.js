e54cee84637ca1da663a2916924547d6091e80a8
yonghun16 <yonghun16@naver.com>
2025-04-14 10:39:13 +0900

    정규식 메소드

--- FrontEnd/Fast_campus/javascript_master/13.정규표현식/src/02.정규식_메소드.js
+++ FrontEnd/Fast_campus/javascript_master/13.정규표현식/src/02.정규식_메소드.js
@@ -1,25 +1,8 @@
 // 정규표현식(RegExp, Regular Expression)
 
-// - 문자 검색(Search)
-// - 문자 대체(Replace)
-// - 문자 추출(Extract)
-
-// 1. 생성자 방식
-// new RegExp('표현', '옵션')
-// new RegExp('[a-z]', 'gi')
-
-// 2. 리터럴 방식
-// /표현/옵션
-// /[a-z]/gi
-
-// 옵션들
-// g : global - 전체 찾기
-// i : ignoreCase - 대소문자 무시
-// m : multiLine - 여러 줄 모드. ^, $ 각각 줄의 시작과 끝을 의미
-// s : dotAll - .이 줄바꿈 문자(\n) 포함한 모든 문자와 일치
-// u : Unicode - 유니코드 모드.
-// y : sticky - 현재 검색 위치에서만 일치하도록 함.
-
+// 정규식.test(문자열) - 이치 여부 반환
+// 문자열.match(정규식) - 일치하는 문자의 배열 반환
+// 문자열.replace(정규식, 대체문자) - 일치하는 문자를 대체
 
 const str = `
 010-1234-5678
@@ -33,8 +16,9 @@ http://localhost:1234
 abbcccddddeeee
 `
 
-const regexp = new RegExp('the', 'gi')
-console.log(str.match(regexp))  // the, The, the
+const regexp = /heropy/gi
+const regexp2 = /fox/gi
 
-const regexp2 = /the/gi
-console.log(str.match(regexp2))  // the, The, the
+console.log(regexp.test(str))   // false
+console.log(str.match(regexp))  // null
+console.log(str.replace(regexp2, 'cat'))
