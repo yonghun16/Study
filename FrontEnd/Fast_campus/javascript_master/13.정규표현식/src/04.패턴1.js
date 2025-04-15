87bb01732d6120eaeda79747433d3d254c6ad03a
yonghun16 <yonghun16@naver.com>
2025-04-15 17:59:23 +0900

    패턴2

--- FrontEnd/Fast_campus/javascript_master/13.정규표현식/src/04.패턴1.js
+++ FrontEnd/Fast_campus/javascript_master/13.정규표현식/src/04.패턴1.js
@@ -17,7 +17,7 @@
 // [0-9]       | 0부터 9 사이의 숫자 구간에 일치(숫자)
 // [가-힣]     | 가부터 힣 사이의 문자 구간에 일치(한글)
 
-// \w          | 63개 문자(Word, 대소영문52개 + 숫자10개 + _)와 일치
+// \w          | 63개 문자( Word( 대소영문52개 + 숫자10개 + _ ) )와 일치
 // \b          | 63개 문자에 일치하지 않는 문자 경계(Boundary)
 // \d          | 숫자와 일치
 // \s          | 공백(space, Tab 등) 와 일치
@@ -39,10 +39,34 @@ http://localhost:1234
 abbcccddddeeee
 `
 
-console.log(str.match(/^h..../gm))    //  h....   // ^줄 시작하는 단어
-console.log(str.match(/\.com/g))      //  .com
-console.log(str.match(/\.com$/gm))    // [ '.com', '.com' ]
-console.log(str.match(/...\.com$/gm)) // [ 'mil.com', 'ver.com' ]
+console.log(str.match(/^h..../gm))        //  h....   // ^줄 시작하는 단어
+console.log(str.match(/\.com/g))          //  .com
+console.log(str.match(/\.com$/gm))        // [ '.com', '.com' ]
+console.log(str.match(/...\.com$/gm))     // [ 'mil.com', 'ver.com' ]
 
-console.log(str.match(/fox|dog|\.com/g))    // [ 'fox', 'dog', '.com', '.com' ]
-console.log(str.match(/https?/g))           // [ 'https', 'http' ]     https?에서 s가 있거나 없거나
+console.log(str.match(/fox|dog|\.com/g))  // [ 'fox', 'dog', '.com', '.com' ]
+console.log(str.match(/https?/g))         // [ 'https', 'http' ]     https?에서 s가 있거나 없거나
+
+console.log(str.match(/\d{3}/g))          // 숫자가 연속 3개인 문자열
+console.log(str.match(/e{3}/g))           // e가 연속 3개인 문자열
+console.log(str.match(/c{3}/g))           // c가 연속 3개인 문자열     // [ 'ccc' ]
+console.log(str.match(/b{3}/g))           // b가 연속 3개인 문자열     // null
+
+console.log(str.match(/\d{3,}/g))         // 3개 연속 일치되는 단위 [ '010', '1234', '5678', '7035', '1234' ]
+console.log(str.match(/\d{2,3}/g))        // 2~3개 연속 일치되는 단위만
+
+console.log(str.match(/\d{1,}/g))         // 숫자가 1개 이상 연속 일치    [ '010', '1234', '5678', '7035', '60', '1234' ]
+console.log(str.match(/\d+/g))            // 숫자가 1개 이상 연속 일치    [ '010', '1234', '5678', '7035', '60', '1234' ]
+
+console.log()
+console.log(str.match(/\b[0-9]+\b/g))     //  | [0-9]+ |
+console.log(str.match(/\s/g))             //  '\n', '\n', '\n', ' ', '\n',
+
+console.log( str.match(/https?:\/\/(?:\w+\.?)+\/?/g) )       //  [ 'https://www.omdbapi.com/', 'http://localhost' ]
+
+console.log(str.match(/.+(?=과)/g))        // 동해물
+console.log(str.match(/.+(?<=과)/g))       // 동해물과
+console.log(str.match(/(?<=과).+/g))       //          백두산이 마르고 닳도록
+
+
+console.log(str.match(/\d{3}-\d{4}-\d{4}/gm))   // 전화번호
