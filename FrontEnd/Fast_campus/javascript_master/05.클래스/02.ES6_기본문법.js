135e1de1b78a20169368f3582d724ee90f7b233e
yonghun16 <yonghun16@naver.com>
2025-04-17 15:10:48 +0900

    Update 02.ES6_기본문법.js

--- FrontEnd/Fast_campus/javascript_master/05.클래스/02.ES6_기본문법.js
+++ FrontEnd/Fast_campus/javascript_master/05.클래스/02.ES6_기본문법.js
@@ -1,19 +1,24 @@
 // ES6 Classes
 
-// 함수를 사용하는 방법
-// 1) 일반 함수 선언으로 객체를 만들고 
-// 2) 만들어진 객체의 프로토 타입에 메소드(함수)를 추가하는 방법
-// function User(first, last) {
-//   this.firstName = first
-//   this.lastName = last
-// }
-// User.prototype.getFullName = function () {
-//   return `${this.firstName} ${this.lastName}`
-// }
+/*
+ 함수를 사용하는 방법
+ 1) 일반 함수 선언으로 객체를 만들고 
+ 2) 만들어진 객체의 프로토 타입에 메소드(함수)를 추가하는 방법
+ function User(first, last) {
+   this.firstName = first
+   this.lastName = last
+ }
+ User.prototype.getFullName = function () {
+   return `${this.firstName} ${this.lastName}`
+ }
+*/
 
-// ES6의 클래스를 이용하는 방법
-// 1) 클래스로 객체를 선언하고 
-// 2) 선언하는 김에 메소드도 같이 만듬
+
+/*
+ ES6의 클래스를 이용하는 방법
+ 1) 클래스로 객체를 선언하고 
+ 2) 선언하는 김에 메소드도 같이 만듬
+*/
 class User {
   constructor(first, last) {
     this.firstName = first
