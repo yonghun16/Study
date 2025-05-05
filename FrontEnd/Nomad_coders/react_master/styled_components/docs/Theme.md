8a2e293eff180ed3df82fedb90be330697e5ad17
yonghun16 <yonghun16@naver.com>
2025-05-05 21:06:16 +0900

    Theme

---
+++ FrontEnd/Nomad_coders/react_master/styled_components/docs/Theme.md
@@ -0,0 +1,54 @@
+# Theme
+
+### Theme
+- 앱 전체에서 일관된 스타일을 적용할 수 있도록 도와주는 전역 스타일 객체
+
+#### Theme의 구조
+```jsx
+// theme.js
+export const theme = {
+  colors: {
+    primary: '#0070f3',
+    secondary: '#1db954',
+    background: '#f4f4f4',
+  },
+  fontSize: {
+    small: '12px',
+    medium: '16px',
+    large: '20px',
+  },
+};
+
+```
+
+#### ThemeProvider로 적용
+```jsx
+// App.js
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+import MyComponent from './MyComponent';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <MyComponent />
+    </ThemeProvider>
+  );
+}
+```
+
+#### styled-components에서 사용
+- styled-component에서 ThemeProvider로 전달받은 Theme를 사용 가능
+```jsx
+// MyComponent.js
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.primary};
+  font-size: ${({ theme }) => theme.fontSize.medium};
+`;
+
+export default function MyComponent() {
+  return <Button>Click me</Button>;
+}
+```
--- FrontEnd/Nomad_coders/react_master/styled_components/src/App.jsx
+++ FrontEnd/Nomad_coders/react_master/styled_components/src/App.jsx
@@ -1,59 +1,24 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+  font-size: 1.5em;
+  text-align: center;
+`;
+
 const Wrapper = styled.div`
   display: flex;
+  background-color: ${(props) => props.theme.backgroundColor};
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
 `
 
-const Emoji = styled.span`
-  font-size: 5rem;
-`;
-
-const animation = keyframes`   // 애니메이션 정의
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0%;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 50%;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0%;
-  }
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${animation} 2s linear infinite;   // 애니메이션 적용
-  ${Emoji} {
-    font-size: 3rem;
-    &:hover {
-      cursor: pointer;
-      font-size: 4rem;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
-`;
-
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="div">😀</Emoji>
-      </Box>
-      <Emoji>🥰</Emoji>
+      <Title>Styled Components</Title>
     </Wrapper>
   );
 }
