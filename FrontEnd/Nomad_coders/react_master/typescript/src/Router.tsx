2b332cb51bca6c9b5edc23f9dd27113ac99085c2
yonghun16 <yonghun16@naver.com>
2025-05-07 21:54:27 +0900

    ErrorElement

--- FrontEnd/Nomad_coders/react_master/typescript/src/Router.tsx
+++ FrontEnd/Nomad_coders/react_master/typescript/src/Router.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./screens/Home";
 import About from "./screens/About";
+import NotFound from "./screens/NotFound";
 import Root from "./Root";
+import ErrorComponent from "./components/ErrorComponent";
 
 const router = createBrowserRouter([
   {
@@ -11,12 +13,14 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+        errorElement: <ErrorComponent />
       },
       {
         path: "/about",
         element: <About />,
       },
     ],
+    errorElement: <NotFound />,
   },
 ]);
 
