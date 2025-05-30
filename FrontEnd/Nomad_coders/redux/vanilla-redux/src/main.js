072c87cf919a3149757f34637904ddad335ea979
yonghun16 <yonghun16@naver.com>
2025-05-30 22:52:55 +0900

    basic counter

--- FrontEnd/Nomad_coders/redux/vanilla-redux/src/main.js
+++ FrontEnd/Nomad_coders/redux/vanilla-redux/src/main.js
@@ -1,6 +1,42 @@
 import './style.css'
+import { createStore } from 'redux';
 
 document.querySelector('#app').innerHTML = `
   <div>
+    <button id="add">Add</button>
+    <span></span>
+    <button id="minus">Minus</button>
   </div>
-`
+`;
+
+// Selectors
+const add = document.querySelector('#add');
+const minus = document.querySelector('#minus');
+const number = document.querySelector('span');
+
+// variables
+let count = 0;
+
+number.innerText = count;
+
+/* Functions */
+// number render
+const updateText = () => {
+  number.innerText = count;
+}
+
+// Handlers
+const handleAdd = () => {
+  count += 1;
+  updateText();
+}
+
+const handleMinus = () => {
+  count -= 1;
+  updateText();
+}
+
+// Event Listeners
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
+
