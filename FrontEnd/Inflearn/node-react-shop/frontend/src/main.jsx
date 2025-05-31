3cb0878640adf7f9a15c1706a05012fe29033156
yonghun16 <yonghun16@naver.com>
2025-06-01 04:03:33 +0900

    redux 준비

--- FrontEnd/Inflearn/node-react-shop/frontend/src/main.jsx
+++ FrontEnd/Inflearn/node-react-shop/frontend/src/main.jsx
@@ -1,8 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import { store } from './store';
 import router from './Router';
 import './index.css'
 
+
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}/>
+  <Provider store={store}>
+    <RouterProvider router={router}/>
+  </Provider>
 )
