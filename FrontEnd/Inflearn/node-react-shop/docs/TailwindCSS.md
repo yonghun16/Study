558ef23be4618fa6eb73d21b2eee30c87c6a929c
yonghun16 <yonghun16@naver.com>
2025-06-01 00:13:30 +0900

    apply tailwind

---
+++ FrontEnd/Inflearn/node-react-shop/docs/TailwindCSS.md
@@ -0,0 +1,5 @@
+# TailWind
+
+```bash
+npm install tailwindcss @tailwindcss/vite
+```
--- FrontEnd/Inflearn/node-react-shop/frontend/src/App.jsx
+++ FrontEnd/Inflearn/node-react-shop/frontend/src/App.jsx
@@ -9,6 +9,9 @@ function App() {
   return (
     <>
       <div>
+        <h1 className="text-3xl font-bold">
+          Hello world!
+        </h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,18 +19,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="bg-white p-8 rounded shadow text-center">
+          <h1 className="text-2xl font-bold text-blue-400 mb-4">Hello Tailwind!</h1>
+          <p className="text-gray-900">Tailwind CSS가 잘 작동합니다.</p>
+          <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            클릭해 보세요
+          </button>
+        </div>
     </>
   )
 }
