d761b90e8ca07981ef0b2f8bb43c365d69e22453
yonghun16 <yonghun16@naver.com>
2025-05-31 23:02:04 +0900

    update

--- FrontEnd/Inflearn/node-react-shop/frontend/eslint.config.js
+++ FrontEnd/Inflearn/node-react-shop/frontend/eslint.config.js
@@ -1,6 +1,5 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
@@ -23,19 +22,13 @@ export default [
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...reactPlugin.configs.recommended.rules,
+       ...reactPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'no-undef': 'error',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
     },
   },
 ]
