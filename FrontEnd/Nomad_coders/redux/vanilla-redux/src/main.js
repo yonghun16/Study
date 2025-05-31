835ba747020db259547049ff9b092d28849323ec
yonghun16 <yonghun16@naver.com>
2025-05-31 13:15:55 +0900

    subscript

--- FrontEnd/Nomad_coders/redux/vanilla-redux/src/main.js
+++ FrontEnd/Nomad_coders/redux/vanilla-redux/src/main.js
@@ -17,6 +17,19 @@ const add = document.querySelector('#add');
 const minus = document.querySelector('#minus');
 const number = document.querySelector('span');
 
+number.innerText = 0;
+
+
+/*
+ * ### 🔁 Redux 데이터 흐름
+ * 1. 컴포넌트에서 액션(dispatch) 발생
+ * 2. 리듀서가 액션에 따라 상태 변경
+ * 3. store가 새로운 상태를 저장
+ * 4. 구독(subscribe)된 컴포넌트가 리렌더링
+*/
+
+
+// 2. 리듀서
 const countModifier = (count = 0, action) => {
   if (action.type === "ADD") {
     return count + 1;
@@ -27,9 +40,31 @@ const countModifier = (count = 0, action) => {
   }
 };
 
+
+// 3. 스토어 생성 
+// 리듀서의 return 값에 의한 변경 저장
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
 
-console.log(countStore.getState());
+// 구독에 쓰일 리스너
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+
+// 4. 구독 등록
+countStore.subscribe(onChange);
+
+
+// 1. dispatch
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" })
+}
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" })
+}
+
+
+// 0.event start
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
