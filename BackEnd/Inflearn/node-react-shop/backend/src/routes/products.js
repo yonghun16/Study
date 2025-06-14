cbb592c67fd12905549f36d7f390c03b04b48062
yonghun16 <yonghun16@naver.com>
2025-06-15 03:48:13 +0900

    더보기

--- BackEnd/Inflearn/node-react-shop/backend/src/routes/products.js
+++ BackEnd/Inflearn/node-react-shop/backend/src/routes/products.js
@@ -31,17 +31,32 @@ router.post('/image', auth, async (req, res, next) => {
 
 // get product
 router.get('/', async (req, res, next) => {
+  // asc 오름차순, desc 내림차순
+  const order = req.query.order ? req.query.order : 'desc';
+  const sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+  const limit = req.query.limit ? Number(req.query.limit) : 20;
+  const skip = req.query.skip ? Number(req.query.skip) : 0;
+
   try {
-    const products = await Product.find().populate('writer');
+    const products = await Product.find()
+      .populate('writer')
+      .sort([[sortBy, order]])
+      .skip(skip)
+      .limit(limit);
+
+    const productsTotal = await Product.countDocuments();
+    const hasMore = skip + limit < productsTotal ? true : false;
+
     return res.status(200).json({
-      products
+      products,
+      hasMore
     });
   } catch (error) {
     next(error);
   }
 })
 
-// auth
+// new product
 router.post('/', auth, async (req, res, next) => {
   try {
     const product = new Product(req.body);
