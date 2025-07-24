fbaed84bf1a90456afe028566403da6b2d925196
yonghun16 <yonghun16@naver.com>
2025-07-24 17:04:29 +0900

    server side

--- FrontEnd/Nomad_coders/nextjs/nextjs14-manual-setup/app/(home)/page.tsx
+++ FrontEnd/Nomad_coders/nextjs/nextjs14-manual-setup/app/(home)/page.tsx
@@ -1,13 +1,22 @@
 import type { Metadata } from 'next';
-
 export const metadata: Metadata = {
-  title: "Home"
+  title: 'Home',
+};
+
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  // return fetch(URL).then(res => res.json());
+  const res = await fetch(URL);
+  const json = await res.json();
+  return json;
 }
 
-export default function Tomato() {
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
-      <h1>Hello</h1>
+      {JSON.stringify(movies)}
     </div>
   )
 }
