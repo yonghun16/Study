b327a7b790d689d96a2b8b0ddb65ae1d8b4253b8
yonghun16 <yonghun16@naver.com>
2025-07-28 19:40:09 +0900

    카트 UI (합계)

--- BackEnd/Inflearn/node-react-shop/frontend/src/pages/CartPage/index.jsx
+++ BackEnd/Inflearn/node-react-shop/frontend/src/pages/CartPage/index.jsx
@@ -1,15 +1,18 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCartItems } from '../../store/thunkFunctions';
 
 const CartPage = () => {
   const userData = useSelector(state => state.user?.userData);
+  const cartDetail = useSelector(state => state.user?.cartDetail);
   const dispatch = useDispatch();
+
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     let cartItemIds = [];
 
-    if(userData?.cart && userData.cart.length > 0) {
+    if (userData?.cart && userData.cart.length > 0) {
       userData.cart.forEach(item => {
         cartItemIds.push(item.id);
       })
@@ -23,8 +26,35 @@ const CartPage = () => {
     }
   }, [dispatch, userData])
 
+  useEffect(() => {
+    calculateTotal(cartDetail);
+  }, [cartDetail])
+
+  const calculateTotal = (cartItems) => {
+    let total = 0;
+    cartItems.map(item => total += item.price * item.quantity)
+    setTotal(total);
+  }
+
   return (
-    <div>CartPage</div>
+    <section>
+      <div className='text-center m-7'>
+        <h2 className='text-2xl'>나의 장바구니</h2>
+
+        {cartDetail?.length > 0 ?
+          <>
+            <div className='mt-10'>
+              <p><span className='font-bold'>합계:</span> {total}원</p>
+              <button className='text-white bg-black hover:bg-gray-500 rounded-md py-2 px-4 mt-50'>
+                구매하기
+              </button>
+            </div>
+          </>
+          :
+          <p>장바구니가 비었습니다.</p>
+        }
+      </div>
+    </section>
   )
 }
 
