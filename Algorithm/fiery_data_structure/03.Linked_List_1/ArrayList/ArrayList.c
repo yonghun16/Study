38d05db27500a81e48ed05667db1f9bfff9e24c0
yonghun16 <yonghun16@naver.com>
2025-11-23 20:17:59 +0900

    Array Linked List

--- Algorithm/fiery_data_structure/03.Linked_List_1/ArrayList/ArrayList.c
+++ Algorithm/fiery_data_structure/03.Linked_List_1/ArrayList/ArrayList.c
@@ -2,48 +2,58 @@
 
 #include <stdio.h>
 
+// 배열 기반 리스트 초기화
 void ListInit(List* plist) {
-    (plist->numOfData) = 0;
-    (plist->curPosition) = -1;
+    (plist->numOfData) = 0;     // 초기화 0개 데이
+    (plist->curPosition) = -1;  // -1은 아무런 위치도 참조하지 않았음을 의미.
 }
 
+// 배열 기반 리스트의 삽입.
 void LInsert(List* plist, LData data) {
-    if (plist->numOfData > LIST_LEN) {
+    if (plist->numOfData > LIST_LEN) {  // 더 이상 저장할 공간이 없다면,
         puts("저장이 불가능합니다.");
         return;
     }
 
-    plist->arr[plist->numOfData] = data;
-    (plist->numOfData)++;
+    plist->arr[plist->numOfData] = data;  // 데이터 저장.
+    (plist->numOfData)++;                 // 저장된 데이터의 수 증가.
 }
 
+// 배열 기반 리스트의 조회.
 int LFirst(List* plist, LData* pdata) {
-    if (plist->numOfData == 0) return FALSE;
+    if (plist->numOfData == 0)  // 저장된 데이터가 하나도 없다면,
+        return FALSE;
 
-    (plist->curPosition) = 0;
-    *pdata = plist->arr[0];
+    (plist->curPosition) = 0;  // 참조 위치 초기화! 처 번째 데이터의 참조를 의미!
+    *pdata = plist->arr[0];    // pdata가 가리키는 공간에 첫 번째 데이터 저장
     return TRUE;
 }
 
-int LNext(List* plist, LData* pdata) {
-    if (plist->curPosition >= (plist->numOfData) - 1) return FALSE;
+int LNext(List* plist, LData* pdata) {                 // 만약에 중간 지점에서 다시 처음부터 참조하기 원한다면? -> 그 다음 데이터 참조
+    if (plist->curPosition >= (plist->numOfData) - 1)  // 더 이상 참조할 데이터가 없다면?
+        return FALSE;
 
     (plist->curPosition)++;
-    *pdata = plist->arr[plist->curPosition];
-    return TRUE;
+    *pdata = plist->arr[plist->curPosition];  // 값의 반환은 매개변수를 통해서!
+    return TRUE;                              // 함수의 반환은 성공여부를 알리기 위해서!
 }
 
+// 배열 기반 리스트의 삭제
+// (삭제되는 데이터는 반환의 과정을 통해서 되돌려주는 것이 옳다!)
 LData LRemove(List* plist) {
-    int rpos = plist->curPosition;
+    int rpos = plist->curPosition;  // 삭제할 데이터의 인덱스 값 참조
     int num = plist->numOfData;
     int i;
-    LData rdata = plist->arr[rpos];
+    LData rdata = plist->arr[rpos];  // 삭제할 데이터를 임시로 저장
 
-    for (i = rpos; i < num - 1; i++) plist->arr[i] = plist->arr[i + 1];
+    for (i = rpos; i < num - 1; i++) {
+        plist->arr[i] = plist->arr[i + 1];
+    }
 
-    (plist->numOfData)--;
-    (plist->curPosition)--;
-    return rdata;
+    (plist->numOfData)--;    // 데이터의 수 감소
+    (plist->curPosition)--;  // 참조위치를 하나 되돌린다. (중요)
+    return rdata;            // 삭제된 데이터의 반화
 }
 
+// 배열 기반 리스트의 데이터의 개수
 int LCount(List* plist) { return plist->numOfData; }
--- Algorithm/fiery_data_structure/03.Linked_List_1/ArrayList/ArrayList.h
+++ Algorithm/fiery_data_structure/03.Linked_List_1/ArrayList/ArrayList.h
@@ -10,20 +10,31 @@ typedef int LData;
 
 typedef struct __ArrayList {
     LData arr[LIST_LEN];
-    int numOfData;
-    int curPosition;
+    int numOfData;    // 리스트에 저장된 데이터의 개수.
+    int curPosition;  // 마지막 참조 위치에 대한 정보 저장.
 } ArrayList;
 
 /*** ArrayList와 관련된 연산들 ****/
 typedef ArrayList List;
 
-void ListInit(List* plist);
-void LInsert(List* plist, LData data);
+void ListInit(List* plist);  // 리스트 초기화
+                             // - 초기화할 리스트의 주소 값을 인자로 전달한다.
+                             // - 리스트 생성 후 제일 먼저 호출되어야 하는 함수이다.
 
-int LFirst(List* plist, LData* pdata);
-int LNext(List* plist, LData* pdata);
+void LInsert(List* plist, LData data);  // 리스트에 데이터를 추가
+                                        // - 리스트에 데이터를 저장한다.
 
-LData LRemove(List* plist);
-int LCount(List* plist);
+int LFirst(List* plist, LData* pdata);  // 저장된 데이터의 탐색 및 탐색 초기화
+                                        // 첫 번째 데이터가 pdata가 가리키는 메모리에 저장된다.
+                                        // - 데이터의 참조를 위한 초기화가 진행된다.
+                                        // - 참조 성공시 TREU(1), 실패 시 FALSE(0) 반환.
+
+int LNext(List* plist, LData* pdata);  // 다음 데이터의 참조(반환)을 목적으로 호출.
+
+LData LRemove(List* plist);  // 바로 이전에 참조(반환)된 데이터를 삭제
+                             // - 삭제된 데이터는 반환된다.
+                             // - 마지막 반환 데이터를 삭제하므로 연이은 반복 호출을 허용하지 않는다.
+
+int LCount(List* plist);  // 현재 저장되어 있는 데이터의 수를 반환
 
 #endif
