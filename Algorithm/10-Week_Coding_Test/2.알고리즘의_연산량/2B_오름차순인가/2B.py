e30c6f194ed4fe62fef47fdf22ad7b30f135124a
yonghun16 <yonghun16@naver.com>
2025-12-28 21:01:41 +0900

    rename

--- Algorithm/10-Week_Coding_Test/2.알고리즘의_연산량/2B_오름차순인가/2B.py
+++ Algorithm/10-Week_Coding_Test/2.알고리즘의_연산량/2B_오름차순인가/2B.py
@@ -1,15 +1,13 @@
 # 오름차순인가?
-
 n = int(input())
 data = list(map(int, input().split()))
 
 previous = 0
-result = 'YES'
+result = "YES"
 
-for i in range(0, n-1):
-    if data[i+1] < data[i]:
-        result = 'NO'
+for i in range(0, n - 1):
+    if data[i + 1] < data[i]:
+        result = "NO"
         break
 
 print(result)
-
