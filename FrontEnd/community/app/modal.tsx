c92ee7ccdff74dc393bcf50a17adcff749e31d4a
yonghun16 <yonghun16@naver.com>
2026-02-16 11:06:54 +0900

    12 프로젝트 초기화

--- FrontEnd/community/app/modal.tsx
+++ FrontEnd/community/app/modal.tsx
@@ -1,17 +1,14 @@
 import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function ModalScreen() {
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
+    <View style={styles.container}>
+      <Text>This is a modal</Text>
       <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
+        <Text>Go to home screen</Text>
       </Link>
-    </ThemedView>
+    </View>
   );
 }
 
