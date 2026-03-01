ae2a3eebd5f8d588119907fbf62eb4bc812cbaa8
yonghun16 <yonghun16@naver.com>
2026-03-01 13:02:45 +0900

    java study

--- BackEnd/Back-end_Books/Java_for_self-study/Chapter01/Hello.java
+++ BackEnd/Back-end_Books/Java_for_self-study/Chapter01/Hello.java
@@ -4,9 +4,9 @@
  */
 
 public class Hello {
-    // 프로그램 실행 진입점
-    public static void main(String[] args){
-        // 콘솔에 출력하는 실행문
-        System.out.println("Hello. Java");
-    } //end of main
-} //end of class
+  // 프로그램 실행 진입점
+  public static void main(String[] args) {
+    // 콘솔에 출력하는 실행문
+    System.out.println("Hello. Java");
+  } // end of main
+} // end of class
