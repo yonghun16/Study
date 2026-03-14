d64e118b3971807f8cd165be55ce673872abe82e
yonghun16 <yonghun16@naver.com>
2026-03-14 11:00:31 +0900

    Forms

--- FrontEnd/Nomad_coders/react_master/state_management/todo_list/src/ToDoList.tsx
+++ FrontEnd/Nomad_coders/react_master/state_management/todo_list/src/ToDoList.tsx
@@ -1,26 +1,49 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+
+// function ToDoList() {
+//   const [todo, setTodo] = useState('');
+//   const [toDoError, setToDoError] = useState('');
+//   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setTodo(value);
+//   };
+//   const onSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     if (todo.length < 10) {
+//       return setToDoError('To do should be longer');
+//     }
+//     console.log("Submit");
+//   };
+//
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={todo} placeholder="Write a to do" />
+//         <button>Add</button>
+//         {toDoError !== "" ? toDoError : null}
+//       </form>
+//     </div>
+//   );
+// }
 
 function ToDoList() {
-  const [todo, setTodo] = useState('');
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setTodo(value);
-  };
-  const onSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(todo);
-  }
-
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} placeholder="Write a to do" />
-        <button>Add</button>
-      </form>
+      <input {...register("email")} placeholder="Email" />
+      <input {...register("first Name")} placeholder="First Name" />
+      <input {...register("last name")} placeholder="Last name" />
+      <input {...register("username")} placeholder="Username" />
+      <input {...register("password")} placeholder="Password" />
+      <input {...register("password1")} placeholder="Password1" />
+      <button>Add</button>
     </div>
-  );
+  )
 }
 
 export default ToDoList;
