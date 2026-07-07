01e76a381cdb08b77c3f2908f6285a7c6aa96abe
yonghun16 <yonghun16@naver.com>
2026-07-07 19:22:53 +0900

    6.12 Refactoring

---
+++ FrontEnd/Nomad_coders/react_master/state_management/todo_list/src/components/ToDo.tsx
@@ -0,0 +1,14 @@
+import type { IToDo } from "./atoms";
+
+function ToDo({ text }: IToDo) {
+  return (
+    <li>
+      <span>{text}</span>
+      <button>Doing</button>
+      <button>To Do</button>
+      <button>Done</button>
+    </ li>
+  );
+}
+
+export default ToDo;
