1c7f1eac25cd57f415d8176386719f1f9372b37c
yonghun16 <yonghun16@naver.com>
2026-07-28 23:00:38 +0900

    update

---
+++ FrontEnd/Nomad_coders/react_master/trello_clone/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { useRecoilState } from 'recoil'
+
+function App() {
+  const [minutes, setMinutes] = useRecoilState(minutesState)
+  return (
+    <>
+      <input type="number" placeholder="Minutes" />
+      <input type="number" placeholder="Hours" />
+    </>
+  )
+}
+
+
+export default App
---
+++ FrontEnd/Nomad_coders/react_master/trello_clone/src/atoms.tsx
@@ -0,0 +1,6 @@
+import { atom, selector } from 'recoil'
+
+export const minutesState = atom({
+  key: 'minutes',
+  default: 0,
+})
